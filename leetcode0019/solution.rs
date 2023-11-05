fb3f61d6450840a11d7a0d1bd972c9a19b99066e
root <root@LAPTOP-6FODDGP0.localdomain>
2023-11-05 22:27:10 +0800

    add solution for leetcode #19 in rust

---
+++ leetcode0019/solution.rs
@@ -0,0 +1,36 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut tmp = Box::new(ListNode{val: 0, next: head});
+        let mut fast = tmp.clone();
+        let mut slow = tmp.as_mut();
+
+        for _ in 0..n {
+            fast = fast.next.unwrap();
+        }
+
+        while fast.next.is_some() {
+            fast = fast.next.unwrap();
+            slow = slow.next.as_mut().unwrap();
+        }
+
+        slow.next = slow.next.as_mut().unwrap().next.clone();
+        tmp.next
+    }
+
+}
