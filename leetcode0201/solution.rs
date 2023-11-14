c0b3f94535037a0579780151b1c2983bb22c850f
root <lintimo1999@gmail.com>
2023-11-15 01:51:09 +0800

    add solution for leetcode #201 in rust

---
+++ leetcode0201/solution.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+        if left == 0 {
+            return 0;
+        }
+
+        if left != right {
+            return Solution::range_bitwise_and(left >> 1, right >> 1) << 1;
+        } else {
+            return right;
+        }
+    }
+}
