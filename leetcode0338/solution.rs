bf8bc70e6d888413de2397fe8dba4b91b370171f
root <lintimo1999@gmail.com>
2023-11-18 22:44:43 +0800

    update solution for leetcode #1838 in rust

--- leetcode0338/solution.rs
+++ leetcode0338/solution.rs
@@ -1,9 +1,18 @@
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
-        let mut ans: Vec<i32> = vec![];
-        for i in 0..=n {
-            ans.push((i).count_ones() as i32);
+        let mut ans: Vec<i32> = vec![0; (n + 1) as usize];
+        for i in 1..=n {
+            ans[i as usize] = ans[(i >> 1) as usize] + (i & 1);
         }
         ans
     }
 }
+// impl Solution {
+//     pub fn count_bits(n: i32) -> Vec<i32> {
+//         let mut ans: Vec<i32> = vec![];
+//         for i in 0..=n {
+//             ans.push((i).count_ones() as i32);
+//         }
+//         ans
+//     }
+// }
