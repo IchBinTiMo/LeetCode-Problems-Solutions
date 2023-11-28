eb2ac7e9971b3c6e1f82beeb3e02a7060b099779
root <lintimo1999@gmail.com>
2023-11-28 20:58:09 +0800

    update solution for leetcode #2147 in rust

--- leetcode2147/solution.rs
+++ leetcode2147/solution.rs
@@ -1,8 +1,9 @@
 impl Solution {
     pub fn number_of_ways(corridor: String) -> i32 {
-        let mut count = 0;
 
-        let mut tmp: Vec<i64> = vec![];
+        let mut ans: i64 = 1;
+
+        let mut count = 0;
 
         let mut last_seat: usize = 0;
 
@@ -16,8 +17,8 @@ impl Solution {
                     if count == 2 {
                         count = 0;
                         last_seat = i;
-                    } else if count == 1 {
-                        tmp.push((i - last_seat) as i64);
+                    } else if count == 1 && last_seat != 0 {
+                        ans = ans * ((i - last_seat) as i64) % 1_000_000_007;
                     }
                 },
                 _ => continue
@@ -32,9 +33,46 @@ impl Solution {
             return 0;
         }
 
-        tmp.remove(0);
-
-
-        (tmp.iter().fold(1, |acc, val| acc * val % 1_000_000_007)) as i32
+        ans as i32
     }
 }
+// impl Solution {
+//     pub fn number_of_ways(corridor: String) -> i32 {
+//         let mut count = 0;
+
+//         let mut tmp: Vec<i64> = vec![];
+
+//         let mut last_seat: usize = 0;
+
+//         let mut total_seats = 0;
+
+//         for (i, cor) in corridor.chars().enumerate() {
+//             match cor {
+//                 'S' => {
+//                     total_seats += 1;
+//                     count += 1;
+//                     if count == 2 {
+//                         count = 0;
+//                         last_seat = i;
+//                     } else if count == 1 {
+//                         tmp.push((i - last_seat) as i64);
+//                     }
+//                 },
+//                 _ => continue
+//             }
+//         }
+
+//         if count == 1 {
+//             return 0;
+//         }
+
+//         if total_seats == 0 {
+//             return 0;
+//         }
+
+//         tmp.remove(0);
+
+
+//         (tmp.iter().fold(1, |acc, val| acc * val % 1_000_000_007)) as i32
+//     }
+// }
