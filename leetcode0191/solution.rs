1652ad20c84d47387735b2f9b6b9efecc7ce83b5
root <lintimo1999@gmail.com>
2023-11-29 13:40:02 +0800

    update solution for leetcode #191 in rust

--- leetcode0191/solution.rs
+++ leetcode0191/solution.rs
@@ -1,5 +1,17 @@
 impl Solution {
-    pub fn hammingWeight (n: u32) -> i32 {
-        n.count_ones() as i32
+    pub fn hammingWeight (mut n: u32) -> i32 {
+        let mut cnt = 0;
+
+        while n > 0 {
+            cnt += (n & 1) as i32;
+            n >>= 1;
+        }
+
+        cnt
     }
 }
+// impl Solution {
+//     pub fn hammingWeight (n: u32) -> i32 {
+//         n.count_ones() as i32
+//     }
+// }
