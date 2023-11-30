d96c3dc1b991465b23eb3d737ee8d8a684d6a538
root <lintimo1999@gmail.com>
2023-11-30 15:43:14 +0800

    update solution for leetcode #1611 in rust

--- leetcode1611/solution.rs
+++ leetcode1611/solution.rs
@@ -1,22 +1,35 @@
 impl Solution {
     pub fn minimum_one_bit_operations(mut n: i32) -> i32 {
         let mut ans: i32 = 0;
-        let mut cnt = 1;
-        let mut flag = 1;
-
+        
         while n > 0 {
-            match (n & 1) {
-                1 => {
-                    ans += ((1 << cnt) - 1) * flag;
-                    flag *= -1;
-                },
-                _ => {} 
-            }
-            cnt += 1;
+            ans ^= n;
             n >>= 1;
         }
-
-
-        ans.abs()
+        
+        ans
     }
 }
+
+// impl Solution {
+//     pub fn minimum_one_bit_operations(mut n: i32) -> i32 {
+//         let mut ans: i32 = 0;
+//         let mut cnt = 1;
+//         let mut flag = 1;
+
+//         while n > 0 {
+//             match (n & 1) {
+//                 1 => {
+//                     ans += ((1 << cnt) - 1) * flag;
+//                     flag *= -1;
+//                 },
+//                 _ => {} 
+//             }
+//             cnt += 1;
+//             n >>= 1;
+//         }
+
+
+//         ans.abs()
+//     }
+// }
