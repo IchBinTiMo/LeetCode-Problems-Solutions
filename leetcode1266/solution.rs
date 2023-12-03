ccc64ca8719bb474a7b4e4470a9d3f79d7a63a46
root <lintimo1999@gmail.com>
2023-12-03 13:10:04 +0800

    update solution for leetcode #1266 in rust

--- leetcode1266/solution.rs
+++ leetcode1266/solution.rs
@@ -1,10 +1,10 @@
 impl Solution {
-    pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+    pub fn min_time_to_visit_all_points(mut points: Vec<Vec<i32>>) -> i32 {
         let mut ans = 0;
         let mut current: Option<Vec<i32>> = None;
 
-        for i in 0..points.len() {
-            let point = points[i].clone();
+        while points.len() > 0 {
+            let point = points.remove(0);
             match current {
                 Some(val) => {
                     ans += ((point[0] - val[0]).abs()).max((point[1] - val[1]).abs());
@@ -17,3 +17,22 @@ impl Solution {
         ans
     }
 }
+// impl Solution {
+//     pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+//         let mut ans = 0;
+//         let mut current: Option<Vec<i32>> = None;
+
+//         for i in 0..points.len() {
+//             let point = points[i].clone();
+//             match current {
+//                 Some(val) => {
+//                     ans += ((point[0] - val[0]).abs()).max((point[1] - val[1]).abs());
+//                     current = Some(point);
+//                 },
+//                 _ => current = Some(point)
+//             }
+//         }
+
+//         ans
+//     }
+// }
