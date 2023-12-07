12855015b0e10bfb9ae228a7e7eab087682665de
root <lintimo1999@gmail.com>
2023-12-07 12:09:05 +0800

    update solution for leetcode #1903 in rust

--- leetcode1903/solution.rs
+++ leetcode1903/solution.rs
@@ -3,7 +3,7 @@ impl Solution {
         let mut right: usize = num.len() - 1;
 
         while right <= 100000 {
-          if (&num[right..=right]).parse::<i32>().unwrap() % 2 == 1 {
+          if (num.as_bytes()[right] - b'0') % 2 == 1 {
               return String::from(&num[0..=right]);
           }
           right -= 1;
@@ -13,3 +13,18 @@ impl Solution {
 
     }
 }
+// impl Solution {
+//     pub fn largest_odd_number(num: String) -> String {
+//         let mut right: usize = num.len() - 1;
+
+//         while right <= 100000 {
+//           if (&num[right..=right]).parse::<i32>().unwrap() % 2 == 1 {
+//               return String::from(&num[0..=right]);
+//           }
+//           right -= 1;
+//         }
+
+//         String::new()
+
+//     }
+// }
