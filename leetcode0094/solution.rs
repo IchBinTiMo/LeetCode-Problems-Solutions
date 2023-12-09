a7923c41fd8562d5e1a1d5c56c6d4b56f0c1bcea
root <lintimo1999@gmail.com>
2023-12-09 14:11:41 +0800

    add solution for leetcode #94 in rust

---
+++ leetcode0094/solution.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut ans: Vec<i32> = vec![];
+        Solution::inorder(root, &mut ans);
+        ans
+    }
+
+    pub fn inorder(root: Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>) {
+        if let Some(node) = root {
+            let nd = node.borrow();
+            Solution::inorder(nd.left.clone(), ans);
+            ans.push(nd.val);
+            Solution::inorder(nd.right.clone(), ans);
+        }
+    }
+}
