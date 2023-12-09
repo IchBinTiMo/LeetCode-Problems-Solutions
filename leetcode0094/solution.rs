74784ea388ac077721b373c5536a6c952dcb3256
root <lintimo1999@gmail.com>
2023-12-09 14:12:52 +0800

    update solution for leetcode #94 in rust

--- leetcode0094/solution.rs
+++ leetcode0094/solution.rs
@@ -21,16 +21,30 @@ use std::cell::RefCell;
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         let mut ans: Vec<i32> = vec![];
-        Solution::inorder(root, &mut ans);
+        if let Some(node) = root {
+            let mut nd = node.borrow_mut();
+            ans.extend(Solution::inorder_traversal(nd.left.take()));
+            ans.push(nd.val);
+            ans.extend(Solution::inorder_traversal(nd.right.take()));
+        }
         ans
     }
-
-    pub fn inorder(root: Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>) {
-        if let Some(node) = root {
-            let nd = node.borrow();
-            Solution::inorder(nd.left.clone(), ans);
-            ans.push(nd.val);
-            Solution::inorder(nd.right.clone(), ans);
-        }
-    }
 }
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// impl Solution {
+//     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+//         let mut ans: Vec<i32> = vec![];
+//         Solution::inorder(root, &mut ans);
+//         ans
+//     }
+
+//     pub fn inorder(root: Option<Rc<RefCell<TreeNode>>>, ans: &mut Vec<i32>) {
+//         if let Some(node) = root {
+//             let nd = node.borrow();
+//             Solution::inorder(nd.left.clone(), ans);
+//             ans.push(nd.val);
+//             Solution::inorder(nd.right.clone(), ans);
+//         }
+//     }
+// }
