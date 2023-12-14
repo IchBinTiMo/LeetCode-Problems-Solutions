655c78dc0a03336b639864d670d476d7ff089cd6
root <lintimo1999@gmail.com>
2023-12-14 11:56:53 +0800

    update solution for leetcode #2482 in rust

--- leetcode2482/solution.rs
+++ leetcode2482/solution.rs
@@ -1,5 +1,5 @@
 impl Solution {
-    pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    pub fn ones_minus_zeros(mut grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let m = grid.len();
         let n = grid[0].len();
 
@@ -8,16 +8,8 @@ impl Solution {
 
         for i in 0..m {
             for j in 0..n {
-                match grid[i][j] {
-                    1 => {
-                        dRows[i] += 1;
-                        dCols[j] += 1;
-                    },
-                    _ => {
-                        dRows[i] -= 1;
-                        dCols[j] -= 1;
-                    }
-                }
+                dRows[i] += grid[i][j];
+                dCols[j] += grid[i][j];
             }
         }
 
@@ -25,33 +17,32 @@ impl Solution {
 
         for i in 0..m {
             for j in 0..n {
-                ans[i][j] = dRows[i] + dCols[j];
+                grid[i][j] = 2 * dRows[i] + 2 * dCols[j] - m as i32 - n as i32;
             }
         }
 
-        ans
+        grid
     }
 }
+
 // impl Solution {
 //     pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 //         let m = grid.len();
 //         let n = grid[0].len();
 
-//         let mut onesRows: Vec<i32> = vec![0; m];
-//         let mut zeroesRows: Vec<i32> = vec![0; m];
-//         let mut onesCols: Vec<i32> = vec![0; n];
-//         let mut zeroesCols: Vec<i32> = vec![0; n];
+//         let mut dRows: Vec<i32> = vec![0; m];
+//         let mut dCols: Vec<i32> = vec![0; n];
 
 //         for i in 0..m {
 //             for j in 0..n {
 //                 match grid[i][j] {
 //                     1 => {
-//                         onesRows[i] += 1;
-//                         onesCols[j] += 1;
+//                         dRows[i] += 1;
+//                         dCols[j] += 1;
 //                     },
 //                     _ => {
-//                         zeroesRows[i] += 1;
-//                         zeroesCols[j] += 1;
+//                         dRows[i] -= 1;
+//                         dCols[j] -= 1;
 //                     }
 //                 }
 //             }
@@ -61,10 +52,47 @@ impl Solution {
 
 //         for i in 0..m {
 //             for j in 0..n {
-//                 ans[i][j] = onesRows[i] + onesCols[j] - zeroesRows[i] - zeroesCols[j];
+//                 ans[i][j] = dRows[i] + dCols[j];
 //             }
 //         }
 
 //         ans
 //     }
 // }
+
+// // impl Solution {
+// //     pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+// //         let m = grid.len();
+// //         let n = grid[0].len();
+
+// //         let mut onesRows: Vec<i32> = vec![0; m];
+// //         let mut zeroesRows: Vec<i32> = vec![0; m];
+// //         let mut onesCols: Vec<i32> = vec![0; n];
+// //         let mut zeroesCols: Vec<i32> = vec![0; n];
+
+// //         for i in 0..m {
+// //             for j in 0..n {
+// //                 match grid[i][j] {
+// //                     1 => {
+// //                         onesRows[i] += 1;
+// //                         onesCols[j] += 1;
+// //                     },
+// //                     _ => {
+// //                         zeroesRows[i] += 1;
+// //                         zeroesCols[j] += 1;
+// //                     }
+// //                 }
+// //             }
+// //         }
+
+// //         let mut ans: Vec<Vec<i32>> = vec![vec![0; n]; m];
+
+// //         for i in 0..m {
+// //             for j in 0..n {
+// //                 ans[i][j] = onesRows[i] + onesCols[j] - zeroesRows[i] - zeroesCols[j];
+// //             }
+// //         }
+
+// //         ans
+// //     }
+// // }
