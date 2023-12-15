ddc6000eab70ec0b0a337b1ccdfb29aa03e17a71
root <lintimo1999@gmail.com>
2023-12-15 12:39:12 +0800

    add solution for leetcode #1436 in rust

---
+++ leetcode1436/solution.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn dest_city(paths: Vec<Vec<String>>) -> String {
+        let mut map: HashMap<String, i32> = HashMap::new();
+
+        for i in 0..paths.len() {
+            let (from, to) = (paths[i][0].clone(), paths[i][1].clone());
+            *map.entry(from.clone()).or_insert(0) += 1;
+            *map.entry(to.clone()).or_insert(0) -= 1;
+
+            if *map.get(&from).unwrap() == 0 {
+                map.remove(&from);
+            }
+
+            if *map.get(&to).unwrap() == 0 {
+                map.remove(&to);
+            }
+        }
+
+        for key in map.keys() {
+            if map[key] < 0 {
+                return String::from(key);
+            }
+        }
+
+        String::new()
+    }
+}
