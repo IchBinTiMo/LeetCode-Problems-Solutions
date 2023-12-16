2828877ec43b1b47f995ff2fe0a29becbc888add
root <lintimo1999@gmail.com>
2023-12-16 13:27:22 +0800

    update solution for leetcode #242 in rust

--- leetcode0242/solution.rs
+++ leetcode0242/solution.rs
@@ -1,27 +1,20 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
         if s.len() != t.len() {
             return false
         }
 
-        let mut tmp: HashMap<u8, i32> = HashMap::new();
+        let mut letters = [0; 26];
 
         for byte in s.bytes() {
-            *tmp.entry(byte).or_insert(0) += 1;
+            letters[(byte - b'a') as usize] += 1;
         }
 
         for byte in t.bytes() {
-            *tmp.entry(byte).or_insert(0) -= 1;
-            if let Some(val) = tmp.get(&byte) {
-                if *val == 0 {
-                    tmp.remove(&byte);
-                }
-            }
+            letters[(byte - b'a') as usize] -= 1;
         }
 
-        tmp.values().len() == 0
+        letters.iter().all(|&l| l == 0)
     }
 }
 
@@ -33,13 +26,40 @@ impl Solution {
 //             return false
 //         }
 
-//         let mut smp: HashMap<char, i32> = HashMap::new();
-//         let mut tmp: HashMap<char, i32> = HashMap::new();
+//         let mut tmp: HashMap<u8, i32> = HashMap::new();
 
-//         for (char_s, char_t) in s.chars().zip(t.chars()) {
-//             *smp.entry(char_s).or_insert(0) += 1;
-//             *tmp.entry(char_t).or_insert(0) += 1;
+//         for byte in s.bytes() {
+//             *tmp.entry(byte).or_insert(0) += 1;
 //         }
-//         smp == tmp
+
+//         for byte in t.bytes() {
+//             *tmp.entry(byte).or_insert(0) -= 1;
+//             if let Some(val) = tmp.get(&byte) {
+//                 if *val == 0 {
+//                     tmp.remove(&byte);
+//                 }
+//             }
+//         }
+
+//         tmp.values().len() == 0
 //     }
 // }
+
+// // use std::collections::HashMap;
+
+// // impl Solution {
+// //     pub fn is_anagram(s: String, t: String) -> bool {
+// //         if s.len() != t.len() {
+// //             return false
+// //         }
+
+// //         let mut smp: HashMap<char, i32> = HashMap::new();
+// //         let mut tmp: HashMap<char, i32> = HashMap::new();
+
+// //         for (char_s, char_t) in s.chars().zip(t.chars()) {
+// //             *smp.entry(char_s).or_insert(0) += 1;
+// //             *tmp.entry(char_t).or_insert(0) += 1;
+// //         }
+// //         smp == tmp
+// //     }
+// // }
