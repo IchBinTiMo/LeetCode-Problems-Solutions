b23b962eb19d6cc1ec5d1ae3681b61633adbddeb
root <lintimo1999@gmail.com>
2023-12-20 14:21:38 +0800

    add solution for leetcode #403 in rust

---
+++ leetcode0403/solution.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn can_cross(stones: Vec<i32>) -> bool {
+        if stones[1] != 1 {
+            return false;
+        }
+        let l = stones.len();
+
+        let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+
+        dp[1][0] = true;
+
+        for i in 1..l {
+            for j in 0..i {
+                if dp[i][j] == false {
+                    continue;
+                }
+                let current = stones[i] - stones[j];
+
+                for k in -1..=1 {
+                    if current + k <= 0 {
+                        continue;
+                    }
+
+                    if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+                        dp[index][i] = true;
+                    }
+
+                }
+            }
+        }
+
+        dp[l - 1].iter().any(|&v| v)
+    }
+}
