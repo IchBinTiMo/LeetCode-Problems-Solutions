6dab883b3605d8ab6523bec03856ca48f8d42778
root <lintimo1999@gmail.com>
2023-12-20 15:15:05 +0800

    update solution for leetcode #403 in rust

--- leetcode0403/solution.rs
+++ leetcode0403/solution.rs
@@ -17,7 +17,7 @@ impl Solution {
 
         dp.push_back((1, 1));
 
-        while let Some((step, des)) = dp.pop_front() {
+        while let Some((step, des)) = dp.pop_back() {
             if visited.get(&(step, des)).is_some() {
                 continue;
             }
@@ -45,6 +45,9 @@ impl Solution {
     }
 }
 
+// use std::collections::VecDeque;
+// use std::collections::HashSet;
+
 // impl Solution {
 //     pub fn can_cross(stones: Vec<i32>) -> bool {
 //         if stones[1] != 1 {
@@ -56,42 +59,49 @@ impl Solution {
 //             return true;
 //         }
 
-//         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+//         let mut dp: VecDeque<(i32, i32)> = VecDeque::new();
+//         let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-//         dp[1][0] = true;
+//         dp.push_back((1, 1));
 
-//         for i in 1..l {
-//             for j in 0..i {
-//                 if dp[i][j] == false {
+//         while let Some((step, des)) = dp.pop_front() {
+//             if visited.get(&(step, des)).is_some() {
+//                 continue;
+//             }
+
+//             visited.insert((step, des));
+
+//             for k in -1..=1 {
+//                 if step + k <= 0 {
 //                     continue;
 //                 }
-//                 let current = stones[i] - stones[j];
 
-//                 for k in -1..=1 {
-//                     if current + k <= 0 {
-//                         continue;
-//                     }
-
-//                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
-//                         if index == l - 1 {
-//                             return true;
-//                         }
-//                         dp[index][i] = true;
-//                     }
-
+//                 if step + k + des == stones[l - 1] {
+//                     return true;
 //                 }
+                
+//                 if stones.contains(&(step + k + des)) {
+//                     dp.push_back((step + k, step + k + des));
+//                 }
+
+
 //             }
 //         }
 
 //         false
 //     }
 // }
+
 // // impl Solution {
 // //     pub fn can_cross(stones: Vec<i32>) -> bool {
 // //         if stones[1] != 1 {
 // //             return false;
 // //         }
 // //         let l = stones.len();
+
+// //         if l == 2 {
+// //             return true;
+// //         }
 
 // //         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
 
@@ -110,6 +120,9 @@ impl Solution {
 // //                     }
 
 // //                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+// //                         if index == l - 1 {
+// //                             return true;
+// //                         }
 // //                         dp[index][i] = true;
 // //                     }
 
@@ -117,6 +130,40 @@ impl Solution {
 // //             }
 // //         }
 
-// //         dp[l - 1].iter().any(|&v| v)
+// //         false
 // //     }
 // // }
+// // // impl Solution {
+// // //     pub fn can_cross(stones: Vec<i32>) -> bool {
+// // //         if stones[1] != 1 {
+// // //             return false;
+// // //         }
+// // //         let l = stones.len();
+
+// // //         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+
+// // //         dp[1][0] = true;
+
+// // //         for i in 1..l {
+// // //             for j in 0..i {
+// // //                 if dp[i][j] == false {
+// // //                     continue;
+// // //                 }
+// // //                 let current = stones[i] - stones[j];
+
+// // //                 for k in -1..=1 {
+// // //                     if current + k <= 0 {
+// // //                         continue;
+// // //                     }
+
+// // //                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+// // //                         dp[index][i] = true;
+// // //                     }
+
+// // //                 }
+// // //             }
+// // //         }
+
+// // //         dp[l - 1].iter().any(|&v| v)
+// // //     }
+// // // }
