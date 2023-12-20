81d78bf9e8265973f529ef613ebc1a516dd17a5e
root <lintimo1999@gmail.com>
2023-12-20 15:15:22 +0800

    update solution for leetcode #403 in rust

--- leetcode0403/solution.rs
+++ leetcode0403/solution.rs
@@ -12,8 +12,8 @@ impl Solution {
             return true;
         }
 
-        let mut dp: VecDeque<(i32, i32)> = VecDeque::new();
-        let mut visited: HashSet<(i32, i32)> = HashSet::new();
+        let mut dp: VecDeque<(i32, usize)> = VecDeque::new();
+        let mut visited: HashSet<(i32, usize)> = HashSet::new();
 
         dp.push_back((1, 1));
 
@@ -24,20 +24,19 @@ impl Solution {
 
             visited.insert((step, des));
 
-            for k in -1..=1 {
-                if step + k <= 0 {
-                    continue;
+            for i in des + 1..l {
+                let diff = stones[i] - stones[des];
+
+                if (diff - step).abs() <= 1 {
+                    if i == l - 1 {
+                        return true;
+                    }
+                    dp.push_back((diff, i));
                 }
 
-                if step + k + des == stones[l - 1] {
-                    return true;
+                if diff - step > 1 {
+                    break;
                 }
-                
-                if stones.contains(&(step + k + des)) {
-                    dp.push_back((step + k, step + k + des));
-                }
-
-
             }
         }
 
@@ -64,7 +63,7 @@ impl Solution {
 
 //         dp.push_back((1, 1));
 
-//         while let Some((step, des)) = dp.pop_front() {
+//         while let Some((step, des)) = dp.pop_back() {
 //             if visited.get(&(step, des)).is_some() {
 //                 continue;
 //             }
@@ -92,6 +91,9 @@ impl Solution {
 //     }
 // }
 
+// // use std::collections::VecDeque;
+// // use std::collections::HashSet;
+
 // // impl Solution {
 // //     pub fn can_cross(stones: Vec<i32>) -> bool {
 // //         if stones[1] != 1 {
@@ -103,42 +105,49 @@ impl Solution {
 // //             return true;
 // //         }
 
-// //         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+// //         let mut dp: VecDeque<(i32, i32)> = VecDeque::new();
+// //         let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-// //         dp[1][0] = true;
+// //         dp.push_back((1, 1));
 
-// //         for i in 1..l {
-// //             for j in 0..i {
-// //                 if dp[i][j] == false {
+// //         while let Some((step, des)) = dp.pop_front() {
+// //             if visited.get(&(step, des)).is_some() {
+// //                 continue;
+// //             }
+
+// //             visited.insert((step, des));
+
+// //             for k in -1..=1 {
+// //                 if step + k <= 0 {
 // //                     continue;
 // //                 }
-// //                 let current = stones[i] - stones[j];
 
-// //                 for k in -1..=1 {
-// //                     if current + k <= 0 {
-// //                         continue;
-// //                     }
-
-// //                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
-// //                         if index == l - 1 {
-// //                             return true;
-// //                         }
-// //                         dp[index][i] = true;
-// //                     }
-
+// //                 if step + k + des == stones[l - 1] {
+// //                     return true;
 // //                 }
+                
+// //                 if stones.contains(&(step + k + des)) {
+// //                     dp.push_back((step + k, step + k + des));
+// //                 }
+
+
 // //             }
 // //         }
 
 // //         false
 // //     }
 // // }
+
 // // // impl Solution {
 // // //     pub fn can_cross(stones: Vec<i32>) -> bool {
 // // //         if stones[1] != 1 {
 // // //             return false;
 // // //         }
 // // //         let l = stones.len();
+
+// // //         if l == 2 {
+// // //             return true;
+// // //         }
 
 // // //         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
 
@@ -157,6 +166,9 @@ impl Solution {
 // // //                     }
 
 // // //                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+// // //                         if index == l - 1 {
+// // //                             return true;
+// // //                         }
 // // //                         dp[index][i] = true;
 // // //                     }
 
@@ -164,6 +176,40 @@ impl Solution {
 // // //             }
 // // //         }
 
-// // //         dp[l - 1].iter().any(|&v| v)
+// // //         false
 // // //     }
 // // // }
+// // // // impl Solution {
+// // // //     pub fn can_cross(stones: Vec<i32>) -> bool {
+// // // //         if stones[1] != 1 {
+// // // //             return false;
+// // // //         }
+// // // //         let l = stones.len();
+
+// // // //         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+
+// // // //         dp[1][0] = true;
+
+// // // //         for i in 1..l {
+// // // //             for j in 0..i {
+// // // //                 if dp[i][j] == false {
+// // // //                     continue;
+// // // //                 }
+// // // //                 let current = stones[i] - stones[j];
+
+// // // //                 for k in -1..=1 {
+// // // //                     if current + k <= 0 {
+// // // //                         continue;
+// // // //                     }
+
+// // // //                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+// // // //                         dp[index][i] = true;
+// // // //                     }
+
+// // // //                 }
+// // // //             }
+// // // //         }
+
+// // // //         dp[l - 1].iter().any(|&v| v)
+// // // //     }
+// // // // }
