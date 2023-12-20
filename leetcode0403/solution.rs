4801933c06bafb3cb0ec10253c548807c68a9728
root <lintimo1999@gmail.com>
2023-12-20 14:28:50 +0800

    upddate solution for leetcode #403 in rust

--- leetcode0403/solution.rs
+++ leetcode0403/solution.rs
@@ -5,6 +5,10 @@ impl Solution {
         }
         let l = stones.len();
 
+        if l == 2 {
+            return true;
+        }
+
         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
 
         dp[1][0] = true;
@@ -22,6 +26,9 @@ impl Solution {
                     }
 
                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+                        if index == l - 1 {
+                            return true;
+                        }
                         dp[index][i] = true;
                     }
 
@@ -29,6 +36,40 @@ impl Solution {
             }
         }
 
-        dp[l - 1].iter().any(|&v| v)
+        false
     }
 }
+// impl Solution {
+//     pub fn can_cross(stones: Vec<i32>) -> bool {
+//         if stones[1] != 1 {
+//             return false;
+//         }
+//         let l = stones.len();
+
+//         let mut dp: Vec<Vec<bool>> = vec![vec![false; l]; l];
+
+//         dp[1][0] = true;
+
+//         for i in 1..l {
+//             for j in 0..i {
+//                 if dp[i][j] == false {
+//                     continue;
+//                 }
+//                 let current = stones[i] - stones[j];
+
+//                 for k in -1..=1 {
+//                     if current + k <= 0 {
+//                         continue;
+//                     }
+
+//                     if let Some(index) = stones.iter().position(|&e| e == current + k + stones[i]) {
+//                         dp[index][i] = true;
+//                     }
+
+//                 }
+//             }
+//         }
+
+//         dp[l - 1].iter().any(|&v| v)
+//     }
+// }
