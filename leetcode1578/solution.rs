6f15783d36939af8d6e509d132699b8f84fbf498
root <lintimo1999@gmail.com>
2023-12-27 14:57:55 +0800

    update solution for leetcode #1578 in rust

--- leetcode1578/solution.rs
+++ leetcode1578/solution.rs
@@ -1,31 +1,61 @@
 impl Solution {
     pub fn min_cost(colors: String, needed_time: Vec<i32>) -> i32 {
 
-        let colors = colors.bytes().collect::<Vec<u8>>();
-        let l = colors.len();
         let mut ans = 0;
 
-        let mut left: usize = 0;
-        let mut right: usize = 1;
+        let mut prev: Option<(char, i32)> = None; // (color, time)>
 
-        while left < l{
-            while right < l && colors[right] == colors[left] {
-                right += 1;
+        for (color, &time) in colors.chars().zip(needed_time.iter()) {
+            match prev {
+                Some((prev_color, prev_time)) => {
+                    if prev_color == color {
+                        prev = Some((color, prev_time.max(time)));
+                    } else {
+                        ans -= prev_time;
+                        prev = Some((color, time));
+                    }
+                },
+                _ => {
+                    prev = Some((color, time));
+                }
             }
-
-            let mut mx = i32::MIN;
-            let sum: i32 = needed_time[left..right].iter().sum();
-
-            while left < right {
-                mx = mx.max(needed_time[left]);
-                left += 1;
-            }
-
-            ans += sum - mx;
-            right += 1;
-
+            ans += time;
         }
+
+        ans -= prev.unwrap().1;
 
         ans
     }
 }
+
+// impl Solution {
+//     pub fn min_cost(colors: String, needed_time: Vec<i32>) -> i32 {
+
+//         let colors = colors.bytes().collect::<Vec<u8>>();
+//         let l = colors.len();
+//         let mut ans = 0;
+
+//         let mut left: usize = 0;
+//         let mut right: usize = 1;
+
+//         while left < l{
+//             while right < l && colors[right] == colors[left] {
+//                 right += 1;
+//             }
+
+//             let mut mx = i32::MIN;
+//             let sum: i32 = needed_time[left..right].iter().sum();
+
+//             while left < right {
+//                 mx = mx.max(needed_time[left]);
+//                 left += 1;
+//             }
+
+//             ans += sum - mx;
+//             right += 1;
+
+//         }
+
+//         ans
+//     }
+// }
