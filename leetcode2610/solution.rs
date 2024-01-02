4ec21527b6ecd5b54dde95d1d955901ec831e710
root <lintimo1999@gmail.com>
2024-01-02 14:34:08 +0800

    update solution for leetcode #2610 in rust

--- leetcode2610/solution.rs
+++ leetcode2610/solution.rs
@@ -1,24 +1,44 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut idx: HashMap<i32, usize> = HashMap::new();
         let mut ans: Vec<Vec<i32>> = Vec::new();
-        ans.push(Vec::new());
-
+        let mut idx: Vec<i32> = vec![0; nums.len() + 1];
+        
         for &num in nums.iter() {
-            if let Some(&i) = idx.get(&num) {
-                if i >= ans.len() {
-                    ans.push(Vec::new());
-                }
-                ans[i].push(num);
-                idx.insert(num, i + 1);
-            } else {
-                ans[0].push(num);
-                idx.insert(num, 1);
+            let cur = idx[num as usize] as usize;
+            if cur >= ans.len() {
+                ans.push(Vec::new());
             }
+
+            ans[cur].push(num);
+            idx[num as usize] += 1;
+
         }
 
         ans
     }
 }
+
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
+//         let mut idx: HashMap<i32, usize> = HashMap::new();
+//         let mut ans: Vec<Vec<i32>> = Vec::new();
+//         ans.push(Vec::new());
+
+//         for &num in nums.iter() {
+//             if let Some(&i) = idx.get(&num) {
+//                 if i >= ans.len() {
+//                     ans.push(Vec::new());
+//                 }
+//                 ans[i].push(num);
+//                 idx.insert(num, i + 1);
+//             } else {
+//                 ans[0].push(num);
+//                 idx.insert(num, 1);
+//             }
+//         }
+
+//         ans
+//     }
+// }
