e868ab3b71857d3ef1e60f17f80a39d199b5b4b9
root <lintimo1999@gmail.com>
2024-01-05 16:00:04 +0800

    update solution for leetcode #300 in rust

--- leetcode0300/solution.rs
+++ leetcode0300/solution.rs
@@ -1,19 +1,48 @@
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
         if nums.len() == 1 {
             return 1;
         }
 
-        let mut dp: Vec<i32> = vec![1; nums.len()];
+        let mut queue: Vec<i32> = Vec::new();
 
-        for i in 0..nums.len() {
-            for j in 0..i {
-                if nums[j] < nums[i] {
-                    dp[i] = dp[i].max(dp[j] + 1);
+        queue.push(nums[0]);
+
+        for i in 1..nums.len() {
+            match nums[i].cmp(queue.last().unwrap()) {
+                Ordering::Equal => continue,
+                Ordering::Greater => queue.push(nums[i]),
+                Ordering::Less => {
+                    match queue.binary_search(&nums[i]) {
+                        Ok(_) => {},
+                        Err(idx) => queue[idx] = nums[i]
+                    }
                 }
             }
         }
 
-        *dp.iter().max().unwrap()
+        queue.len() as i32
     }
 }
+
+// impl Solution {
+//     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+//         if nums.len() == 1 {
+//             return 1;
+//         }
+
+//         let mut dp: Vec<i32> = vec![1; nums.len()];
+
+//         for i in 0..nums.len() {
+//             for j in 0..i {
+//                 if nums[j] < nums[i] {
+//                     dp[i] = dp[i].max(dp[j] + 1);
+//                 }
+//             }
+//         }
+
+//         *dp.iter().max().unwrap()
+//     }
+// }
