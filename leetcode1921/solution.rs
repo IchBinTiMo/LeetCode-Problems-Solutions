8a9f64d2a72527d2786060edee4c27e5f38488a2
root <lintimo1999@gmail.com>
2024-01-13 21:56:57 +0800

    update solution for leetcode #1921 in rust

--- leetcode1921/solution.rs
+++ leetcode1921/solution.rs
@@ -19,3 +19,25 @@ impl Solution {
         ans 
     }
 }
+
+// impl Solution {
+//     pub fn eliminate_maximum(mut dist: Vec<i32>, speed: Vec<i32>) -> i32 {
+//         let mut ans = 1;
+
+//         let mut monsters: Vec<i32> = Vec::new();
+
+//         for (m, s) in dist.iter().zip(speed.iter()) {
+//             monsters.push((*m as f64 / *s as f64).ceil() as i32 );
+//         }
+
+//         monsters.sort();
+//         monsters.remove(0);
+
+//         while monsters.len() > 0 && monsters[0] - ans > 0 {
+//             ans += 1;
+//             monsters.remove(0);
+//         }
+
+//         ans 
+//     }
+// }
