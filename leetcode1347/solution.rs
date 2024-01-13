fd91794c91fec9d15b32f66060fdecf653495956
root <lintimo1999@gmail.com>
2024-01-13 21:44:28 +0800

    update solution for leetcode #1347 in rust

--- leetcode1347/solution.rs
+++ leetcode1347/solution.rs
@@ -1,23 +1,43 @@
-use std::collections::HashMap;
 
 impl Solution {
     pub fn min_steps(s: String, t: String) -> i32 {
-        let mut map: HashMap<u8, i32> = HashMap::new();
+
+        let mut c1: [i32; 26] = [0; 26];
 
         for byte in s.bytes() {
-            *map.entry(byte).or_insert(0) += 1;
+            c1[(byte - b'a') as usize] += 1;
         }
-
 
         for byte in t.bytes() {
-            if let Some(cnt) = map.get_mut(&byte) {
-                *cnt -= 1;
-                if *cnt == 0 {
-                    map.remove(&byte);
-                }
-            } 
+            if c1[(byte - b'a') as usize] > 0 {
+                c1[(byte - b'a') as usize] -= 1;
+            }
         }
 
-        map.values().sum::<i32>()
+        c1.iter().sum()
     }
 }
+
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn min_steps(s: String, t: String) -> i32 {
+//         let mut map: HashMap<u8, i32> = HashMap::new();
+
+//         for byte in s.bytes() {
+//             *map.entry(byte).or_insert(0) += 1;
+//         }
+
+
+//         for byte in t.bytes() {
+//             if let Some(cnt) = map.get_mut(&byte) {
+//                 *cnt -= 1;
+//                 if *cnt == 0 {
+//                     map.remove(&byte);
+//                 }
+//             } 
+//         }
+
+//         map.values().sum::<i32>()
+//     }
+// }
