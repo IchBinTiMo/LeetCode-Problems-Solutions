e48219ae7f08e1c20a2def657b6bce4549ada702
root <lintimo1999@gmail.com>
2024-01-14 12:55:02 +0800

    update solution for leetcode #1657 in rust

--- leetcode1657/solution.rs
+++ leetcode1657/solution.rs
@@ -1,21 +1,20 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn close_strings(word1: String, word2: String) -> bool {
         let mut cnt_by_chars1: [i32; 26] = [0; 26];
         let mut cnt_by_chars2: [i32; 26] = [0; 26];
 
-        let mut visited1: HashSet<u8> = HashSet::new();
-        let mut visited2: HashSet<u8> = HashSet::new();
-
         for byte in word1.bytes() {
             cnt_by_chars1[(byte - b'a') as usize] += 1;
-            visited1.insert(byte);
         }
         
         for byte in word2.bytes() {
             cnt_by_chars2[(byte - b'a') as usize] += 1;
-            visited2.insert(byte);
+        }
+
+        for i in 0..26 {
+            if (cnt_by_chars1[i] == 0) != (cnt_by_chars2[i] == 0) {
+                return false;
+            }
         }
 
         let mut cnt_by_cnt1: [i32; 100001] = [0; 100001];
@@ -29,8 +28,43 @@ impl Solution {
             cnt_by_cnt2[cnt as usize] += 1;
         }
 
-        cnt_by_cnt1 == cnt_by_cnt2 && visited1 == visited2
-
-
+        cnt_by_cnt1 == cnt_by_cnt2
     }
 }
+
+// use std::collections::HashSet;
+
+// impl Solution {
+//     pub fn close_strings(word1: String, word2: String) -> bool {
+//         let mut cnt_by_chars1: [i32; 26] = [0; 26];
+//         let mut cnt_by_chars2: [i32; 26] = [0; 26];
+
+//         let mut visited1: HashSet<u8> = HashSet::new();
+//         let mut visited2: HashSet<u8> = HashSet::new();
+
+//         for byte in word1.bytes() {
+//             cnt_by_chars1[(byte - b'a') as usize] += 1;
+//             visited1.insert(byte);
+//         }
+        
+//         for byte in word2.bytes() {
+//             cnt_by_chars2[(byte - b'a') as usize] += 1;
+//             visited2.insert(byte);
+//         }
+
+//         let mut cnt_by_cnt1: [i32; 100001] = [0; 100001];
+//         let mut cnt_by_cnt2: [i32; 100001] = [0; 100001];
+
+//         for &cnt in cnt_by_chars1.iter() {
+//             cnt_by_cnt1[cnt as usize] += 1;
+//         }
+
+//         for &cnt in cnt_by_chars2.iter() {
+//             cnt_by_cnt2[cnt as usize] += 1;
+//         }
+
+//         cnt_by_cnt1 == cnt_by_cnt2 && visited1 == visited2
+
+
+//     }
+// }
