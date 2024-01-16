265a0e8c40e691f3bf42aa6019c4b23234f3d804
root <lintimo1999@gmail.com>
2024-01-16 21:28:10 +0800

    update solution for leetcode #2366 in rust

--- leetcode2366/solution.rs
+++ leetcode2366/solution.rs
@@ -1,40 +1,51 @@
 impl Solution {
     pub fn minimum_replacement(nums: Vec<i32>) -> i64 {
-        // let mut length = (nums.len() - 1) as i32;
-        let mut step:i64 = 0;
-        let mut last:i64 = nums[nums.len() - 1] as i64;
+        let mut ans: i64 = 0;
 
-        for num in nums.iter().rev() {
-            if last >= (*num as i64) {
-                last = (*num as i64);
+        let mut current = *nums.last().unwrap();
+
+        for &num in nums.iter().rev() {
+            if current >= num {
+                current = num;
                 continue;
             }
 
-            let mut tmp:i64 = ((*num as i64) / last) as i64;
+            let mut step: i64 = match num % current {
+                0 => (num / current - 1) as i64,
+                _ => (num / current) as i64
+            };
 
-            if ((*num as i64) % last) > 0 {
-                tmp += 1;
-            }
+            current = (num as i64 / (step + 1)) as i32;
 
-            last = ((*num as i64) / tmp) as i64;
+            ans += step;
+        }
 
-            step += tmp-1;
-            // println!("{}, {}", i, nums[i]);
-            // if last >= nums[i] {
-            //     last = nums[i];
-            //     continue;
-            // }
-            // let mut tmp:i32 = (nums[i - 1] / last) as i32;
-
-            // if (nums[i - 1] % last) > 0 {
-            //     tmp += 1;
-            // }
-
-            // last = (nums[i - 1] / tmp) as i32;
-
-            // step += tmp
-
-        } 
-        step as i64
+        ans
     }
 }
+
+// impl Solution {
+//     pub fn minimum_replacement(nums: Vec<i32>) -> i64 {
+//         // let mut length = (nums.len() - 1) as i32;
+//         let mut step:i64 = 0;
+//         let mut last:i64 = nums[nums.len() - 1] as i64;
+
+//         for num in nums.iter().rev() {
+//             if last >= (*num as i64) {
+//                 last = (*num as i64);
+//                 continue;
+//             }
+
+//             let mut tmp:i64 = ((*num as i64) / last) as i64;
+
+//             if ((*num as i64) % last) > 0 {
+//                 tmp += 1;
+//             }
+
+//             last = ((*num as i64) / tmp) as i64;
+
+//             step += tmp-1;
+//         } 
+//         step as i64
+//     }
+// }
