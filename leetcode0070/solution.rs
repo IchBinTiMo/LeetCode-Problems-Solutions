833d5f79c9454d6260a2e8e2c3f1a8187b4a7378
root <lintimo1999@gmail.com>
2024-01-18 14:33:45 +0800

    update solution for leetcode #70 in rust

--- leetcode0070/solution.rs
+++ leetcode0070/solution.rs
@@ -1,14 +1,25 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        let mut prev = 1;
-        let mut current = 1;
-
-        for i in 2..=n {
-            let tmp = current;
-            current = prev + current;
-            prev = tmp;
+        let (mut prev, mut current) = (1, 1);
+        for _ in 2..=n {
+            (prev, current) = (current, prev + current);
         }
 
         current
     }
 }
+
+// impl Solution {
+//     pub fn climb_stairs(n: i32) -> i32 {
+//         let mut prev = 1;
+//         let mut current = 1;
+
+//         for i in 2..=n {
+//             let tmp = current;
+//             current = prev + current;
+//             prev = tmp;
+//         }
+
+//         current
+//     }
+// }
