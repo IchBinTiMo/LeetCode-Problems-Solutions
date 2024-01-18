147776388b9d0e74fb0a8209150fbf9d78471d9c
root <lintimo1999@gmail.com>
2024-01-18 14:41:40 +0800

    update solution for leetcode #70 in javascript

--- leetcode0070/solution.js
+++ leetcode0070/solution.js
@@ -2,15 +2,32 @@
  * @param {number} n
  * @return {number}
  */
-
-let cache = [];
 var climbStairs = function(n) {
-    if(n <= 3){
-        cache.push(n);
-        return n;
+    let prev = 1;
+    let current = 1;
+
+    for(let i = 2; i <= n; i++) {
+        let tmp = current;
+        current = prev + current;
+        prev = tmp;
     }
-    if(!cache[n]){
-        cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
-    }
-    return cache[n];
-}
+
+    return current;
+};
+
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+
+// let cache = [];
+// var climbStairs = function(n) {
+//     if(n <= 3){
+//         cache.push(n);
+//         return n;
+//     }
+//     if(!cache[n]){
+//         cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+//     return cache[n];
+// }
