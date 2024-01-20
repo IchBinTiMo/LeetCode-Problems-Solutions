5129a59abbe2722613671f1df2c203f933e2cf56
root <lintimo1999@gmail.com>
2024-01-20 16:48:37 +0800

    add solution for leetcode #931 in rust

--- leetcode0931/solution.rs
+++ leetcode0931/solution.rs
@@ -1,14 +1,19 @@
 impl Solution {
     pub fn min_falling_path_sum(mut matrix: Vec<Vec<i32>>) -> i32 {
-        let n = matrix.len() - 1;
+        let n: usize = matrix.len() - 1;
+
         for r in 1..=n {
             for c in 0..=n {
-                if c == 0 {
-                    matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c + 1]);
-                } else if c == n {
-                    matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c - 1]);
-                } else {
-                    matrix[r][c] += matrix[r - 1][c - 1].min(matrix[r - 1][c].min(matrix[r - 1][c + 1]));
+                match c {
+                    0 => {
+                        matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c + 1]);
+                    },
+                    x if x == n => {
+                        matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c - 1]);
+                    },
+                    _ => {
+                        matrix[r][c] += matrix[r - 1][c - 1].min(matrix[r - 1][c].min(matrix[r - 1][c + 1]));
+                    },
                 }
             }
         }
@@ -16,3 +21,22 @@ impl Solution {
         *matrix[n].iter().min().unwrap()
     }
 }
+
+// impl Solution {
+//     pub fn min_falling_path_sum(mut matrix: Vec<Vec<i32>>) -> i32 {
+//         let n = matrix.len() - 1;
+//         for r in 1..=n {
+//             for c in 0..=n {
+//                 if c == 0 {
+//                     matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c + 1]);
+//                 } else if c == n {
+//                     matrix[r][c] += matrix[r - 1][c].min(matrix[r - 1][c - 1]);
+//                 } else {
+//                     matrix[r][c] += matrix[r - 1][c - 1].min(matrix[r - 1][c].min(matrix[r - 1][c + 1]));
+//                 }
+//             }
+//         }
+
+//         *matrix[n].iter().min().unwrap()
+//     }
+// }
