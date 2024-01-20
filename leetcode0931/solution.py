78e4eeaddc83b7b623a6001ee8e8bacd6c536d6e
root <lintimo1999@gmail.com>
2024-01-20 16:47:38 +0800

    add solution for leetcode #931 in python

---
+++ leetcode0931/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        for row in range(1, len(matrix)):
+            for col in range(len(matrix[0])):
+                matrix[row][col] += min(matrix[row - 1][max(0, col - 1):col + 2])
+
+        return min(matrix[-1])
