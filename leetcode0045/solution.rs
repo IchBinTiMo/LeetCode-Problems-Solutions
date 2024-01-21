f036750dff03b291d88003377904e986c189eb3e
root <lintimo1999@gmail.com>
2024-01-22 00:05:06 +0800

    update solution for leetcode #45 in rust

--- leetcode0045/solution.rs
+++ leetcode0045/solution.rs
@@ -1,22 +1,42 @@
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
-        let n: usize = nums.len();
-        let mut dp: Vec<i32> = vec![i32::MAX; n];
 
-        dp[0] = 0;
+        let mut ans: i32 = 0;
+        let mut mx: i32 = 0;
+        let mut limit: i32 = 0;
 
-        for i in 0..n {
-            for j in 1..=nums[i] {
-                let j = j as usize;
-                if i + j >= n {
-                    break;
-                }
-                dp[i + j] = dp[i + j].min(dp[i] + 1);
+        for i in 0..(nums.len() - 1) {
+            mx = mx.max(nums[i] + i as i32);
+            if i as i32 >= limit {
+                ans += 1;
+                limit = mx;
             }
         }
 
-
-        *dp.last().unwrap()
+        ans
 
     }
 }
+
+// impl Solution {
+//     pub fn jump(nums: Vec<i32>) -> i32 {
+//         let n: usize = nums.len();
+//         let mut dp: Vec<i32> = vec![i32::MAX; n];
+
+//         dp[0] = 0;
+
+//         for i in 0..n {
+//             for j in 1..=nums[i] {
+//                 let j = j as usize;
+//                 if i + j >= n {
+//                     break;
+//                 }
+//                 dp[i + j] = dp[i + j].min(dp[i] + 1);
+//             }
+//         }
+
+
+//         *dp.last().unwrap()
+
+//     }
+// }
