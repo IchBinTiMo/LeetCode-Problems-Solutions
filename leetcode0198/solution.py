32054aa2ffd7e2b803102b7b8529fe2ecf665f4e
root <lintimo1999@gmail.com>
2024-01-21 14:36:06 +0800

    add solution for leetcode #198 in python

---
+++ leetcode0198/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if(len(nums) <= 2):
+            return max(nums)
+        if len(nums) % 2:
+            nums.append(0)
+        for i in range(1, int(len(nums) / 2)):
+            if i >= 2:
+                nums[i * 2] += max(nums[(i - 1) * 2], nums[(i - 2) * 2 + 1])
+            else:
+                nums[i * 2] += nums[(i - 1) * 2]
+            nums[i * 2 + 1] += max(nums[(i - 1) * 2] , nums[(i - 1) * 2 + 1])
+
+          
+        return max(nums[-1], nums[-2])
