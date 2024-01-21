1f9ace5259d25d6b3c58eb7403c245cff0b97104
root <lintimo1999@gmail.com>
2024-01-21 14:35:18 +0800

    update solution for leetcode #198 in rust

--- leetcode0198/solution.rs
+++ leetcode0198/solution.rs
@@ -2,12 +2,26 @@ impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
         let mut n: usize = nums.len();
 
-        let mut dp: Vec<i32> = vec![0; n + 2];
+        let (mut prev_prev, mut prev) = (0_i32, 0_i32);
 
         for i in 0..n {
-            dp[i + 2] = dp[i + 1].max(dp[i] + nums[i]);
+            (prev_prev, prev) = (prev, prev.max(prev_prev + nums[i]));
         }
 
-        *dp.last().unwrap()
+        prev
     }
 }
+
+// impl Solution {
+//     pub fn rob(nums: Vec<i32>) -> i32 {
+//         let mut n: usize = nums.len();
+
+//         let mut dp: Vec<i32> = vec![0; n + 2];
+
+//         for i in 0..n {
+//             dp[i + 2] = dp[i + 1].max(dp[i] + nums[i]);
+//         }
+
+//         *dp.last().unwrap()
+//     }
+// }
