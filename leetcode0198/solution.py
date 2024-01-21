edba7f8895059149bf33acc72ebb1246f844c733
root <lintimo1999@gmail.com>
2024-01-21 14:36:33 +0800

    update solution for leetcode #198 in python

--- leetcode0198/solution.py
+++ leetcode0198/solution.py
@@ -1,15 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if(len(nums) <= 2):
-            return max(nums)
-        if len(nums) % 2:
-            nums.append(0)
-        for i in range(1, int(len(nums) / 2)):
-            if i >= 2:
-                nums[i * 2] += max(nums[(i - 1) * 2], nums[(i - 2) * 2 + 1])
-            else:
-                nums[i * 2] += nums[(i - 1) * 2]
-            nums[i * 2 + 1] += max(nums[(i - 1) * 2] , nums[(i - 1) * 2 + 1])
+        prev_prev= 0
+        prev = nums[0]
+
+        for i in range(1, len(nums)):
+            prev_prev, prev = prev, max(prev, prev_prev + nums[i])
+
+        return prev
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if(len(nums) <= 2):
+#             return max(nums)
+#         if len(nums) % 2:
+#             nums.append(0)
+#         for i in range(1, int(len(nums) / 2)):
+#             if i >= 2:
+#                 nums[i * 2] += max(nums[(i - 1) * 2], nums[(i - 2) * 2 + 1])
+#             else:
+#                 nums[i * 2] += nums[(i - 1) * 2]
+#             nums[i * 2 + 1] += max(nums[(i - 1) * 2] , nums[(i - 1) * 2 + 1])
 
           
-        return max(nums[-1], nums[-2])
+#         return max(nums[-1], nums[-2])
