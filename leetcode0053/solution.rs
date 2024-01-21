b38580e8697dac0bbdcfc6d56d8c565212ff9f42
root <lintimo1999@gmail.com>
2024-01-22 00:36:43 +0800

    update solution for leetcode #53 in rust

--- leetcode0053/solution.rs
+++ leetcode0053/solution.rs
@@ -2,19 +2,36 @@ impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         let n: usize = nums.len();
         let mut dp: Vec<i32> = vec![i32::MIN; n];
+        let mut ans: i32 = nums[0];
 
         dp[0] = nums[0];
 
         for i in 1..n {
             dp[i] = nums[i].max((dp[i - 1] + nums[i]).max(nums[i]));
-        }
-
-        let mut ans: i32 = i32::MIN;
-
-        while let Some(val) = dp.pop() {
-            ans = ans.max(val);
+            ans = ans.max(dp[i]);
         }
 
         ans
     }
 }
+
+// impl Solution {
+//     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+//         let n: usize = nums.len();
+//         let mut dp: Vec<i32> = vec![i32::MIN; n];
+
+//         dp[0] = nums[0];
+
+//         for i in 1..n {
+//             dp[i] = nums[i].max((dp[i - 1] + nums[i]).max(nums[i]));
+//         }
+
+//         let mut ans: i32 = i32::MIN;
+
+//         while let Some(val) = dp.pop() {
+//             ans = ans.max(val);
+//         }
+
+//         ans
+//     }
+// }
