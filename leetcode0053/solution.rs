4bda84c9ff623caf77080d4a54a80e8bc3b80855
root <lintimo1999@gmail.com>
2024-01-22 00:38:03 +0800

    update solution for leetcode #53 in rust

--- leetcode0053/solution.rs
+++ leetcode0053/solution.rs
@@ -3,9 +3,8 @@ impl Solution {
         let mut ans: i32 = i32::MIN;
         let mut current: i32 = 0;
 
-
-        for i in 0..nums.len() {
-            current = (current + nums[i]).max(nums[i]);
+        for &num in nums.iter() {
+            current = (current + num).max(num);
             ans = ans.max(current);
         }
 
@@ -15,15 +14,13 @@ impl Solution {
 
 // impl Solution {
 //     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-//         let n: usize = nums.len();
-//         let mut dp: Vec<i32> = vec![i32::MIN; n];
-//         let mut ans: i32 = nums[0];
+//         let mut ans: i32 = i32::MIN;
+//         let mut current: i32 = 0;
 
-//         dp[0] = nums[0];
 
-//         for i in 1..n {
-//             dp[i] = nums[i].max(dp[i - 1] + nums[i]);
-//             ans = ans.max(dp[i]);
+//         for i in 0..nums.len() {
+//             current = (current + nums[i]).max(nums[i]);
+//             ans = ans.max(current);
 //         }
 
 //         ans
@@ -39,7 +36,7 @@ impl Solution {
 // //         dp[0] = nums[0];
 
 // //         for i in 1..n {
-// //             dp[i] = nums[i].max((dp[i - 1] + nums[i]).max(nums[i]));
+// //             dp[i] = nums[i].max(dp[i - 1] + nums[i]);
 // //             ans = ans.max(dp[i]);
 // //         }
 
@@ -51,19 +48,36 @@ impl Solution {
 // // //     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
 // // //         let n: usize = nums.len();
 // // //         let mut dp: Vec<i32> = vec![i32::MIN; n];
+// // //         let mut ans: i32 = nums[0];
 
 // // //         dp[0] = nums[0];
 
 // // //         for i in 1..n {
 // // //             dp[i] = nums[i].max((dp[i - 1] + nums[i]).max(nums[i]));
-// // //         }
-
-// // //         let mut ans: i32 = i32::MIN;
-
-// // //         while let Some(val) = dp.pop() {
-// // //             ans = ans.max(val);
+// // //             ans = ans.max(dp[i]);
 // // //         }
 
 // // //         ans
 // // //     }
 // // // }
+
+// // // // impl Solution {
+// // // //     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+// // // //         let n: usize = nums.len();
+// // // //         let mut dp: Vec<i32> = vec![i32::MIN; n];
+
+// // // //         dp[0] = nums[0];
+
+// // // //         for i in 1..n {
+// // // //             dp[i] = nums[i].max((dp[i - 1] + nums[i]).max(nums[i]));
+// // // //         }
+
+// // // //         let mut ans: i32 = i32::MIN;
+
+// // // //         while let Some(val) = dp.pop() {
+// // // //             ans = ans.max(val);
+// // // //         }
+
+// // // //         ans
+// // // //     }
+// // // // }
