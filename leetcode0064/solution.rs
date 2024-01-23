694f7ca2a2201a5d54370e85a7a576ddcdd5118b
root <lintimo1999@gmail.com>
2024-01-23 21:39:36 +0800

    update solution for leetcode #64 in rust

--- leetcode0064/solution.rs
+++ leetcode0064/solution.rs
@@ -1,25 +1,46 @@
 impl Solution {
-    pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
 
         let m: usize = grid.len();
         let n: usize = grid[0].len();
-
-        let mut dp: Vec<Vec<i32>> = vec![vec![0; n]; m];
-
-        dp[0][0] = grid[0][0];
-
 
         for i in 0..m {
             for j in 0..n {
                 match (i, j) {
                     (0, 0) => continue,
-                    (0, _) => dp[i][j] += grid[i][j] + dp[i][j - 1],
-                    (_, 0) => dp[i][j] += grid[i][j] + dp[i - 1][j],
-                    (_, _) => dp[i][j] += grid[i][j] + dp[i][j - 1].min(dp[i - 1][j])
+                    (0, _) => grid[i][j] += grid[i][j - 1],
+                    (_, 0) => grid[i][j] += grid[i - 1][j],
+                    (_, _) => grid[i][j] += grid[i][j - 1].min(grid[i - 1][j])
                 }
             }
         }
 
-        dp[m - 1][n - 1]
+        grid[m - 1][n - 1]
     }
 }
+
+// impl Solution {
+//     pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+
+//         let m: usize = grid.len();
+//         let n: usize = grid[0].len();
+
+//         let mut dp: Vec<Vec<i32>> = vec![vec![0; n]; m];
+
+//         dp[0][0] = grid[0][0];
+
+
+//         for i in 0..m {
+//             for j in 0..n {
+//                 match (i, j) {
+//                     (0, 0) => continue,
+//                     (0, _) => dp[i][j] += grid[i][j] + dp[i][j - 1],
+//                     (_, 0) => dp[i][j] += grid[i][j] + dp[i - 1][j],
+//                     (_, _) => dp[i][j] += grid[i][j] + dp[i][j - 1].min(dp[i - 1][j])
+//                 }
+//             }
+//         }
+
+//         dp[m - 1][n - 1]
+//     }
+// }
