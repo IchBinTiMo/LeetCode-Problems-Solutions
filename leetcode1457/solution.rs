c1defec35c412eedd16c7871507317948d9618df
root <lintimo1999@gmail.com>
2024-01-24 15:08:14 +0800

    add solution for leetcode #1457 in rust

---
+++ leetcode1457/solution.rs
@@ -0,0 +1,55 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn pseudo_palindromic_paths (root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut ans: i32 = 0;
+
+        let mut occ: i32 = 0;
+
+        Solution::dfs(&mut ans, root, &mut occ);
+
+        ans
+    }
+
+    pub fn dfs(ans: &mut i32, root: Option<Rc<RefCell<TreeNode>>>, occ: &mut i32) {
+        if let Some(node) = root {
+            let n = node.borrow();
+
+            *occ = *occ ^ (1 << n.val);
+
+            if n.left.is_none() && n.right.is_none() {
+
+                if *occ & (*occ - 1) == 0 {
+                    *ans += 1;
+                }
+
+                *occ = *occ ^ (1 << n.val);
+
+                return;
+            }
+
+            Solution::dfs(ans, n.left.clone(), occ);
+            Solution::dfs(ans, n.right.clone(), occ);
+         
+            *occ = *occ ^ (1 << n.val);
+        }
+    }
+}
