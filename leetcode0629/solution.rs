76a84ead879a7dc7412e14885debcfd35e237ce4
root <lintimo1999@gmail.com>
2024-01-27 16:36:34 +0800

    update solution for leetcode #629 in rust

--- leetcode0629/solution.rs
+++ leetcode0629/solution.rs
@@ -8,7 +8,8 @@ impl Solution {
 
         for i in 2..=n {
             let mut dp: Vec<i32> = vec![0; (k + 1) as usize];
-            for j in 0..=k {
+            dp[0] = 1;
+            for j in 1..=k {
                 for x in (j - i + 1).max(0)..=j {
                     let x = x as usize;
                     let j = j as usize;
@@ -22,3 +23,28 @@ impl Solution {
         *prev.last().unwrap()
     }
 }
+
+// impl Solution {
+//     pub fn k_inverse_pairs(n: i32, k: i32) -> i32 {
+//         let mut ans = 0;
+
+//         let mut prev: Vec<i32> = vec![0; (k + 1) as usize];
+
+//         prev[0] = 1;
+
+//         for i in 2..=n {
+//             let mut dp: Vec<i32> = vec![0; (k + 1) as usize];
+//             for j in 0..=k {
+//                 for x in (j - i + 1).max(0)..=j {
+//                     let x = x as usize;
+//                     let j = j as usize;
+//                     dp[j] = (dp[j] + prev[x]) % 1_000_000_007;
+//                 }
+//             }
+//             prev = dp;
+//         }
+
+
+//         *prev.last().unwrap()
+//     }
+// }
