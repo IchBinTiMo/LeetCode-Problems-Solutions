9a5269ef08f933577a33e0123595144ad0ada36d
root <lintimo1999@gmail.com>
2024-01-30 09:02:44 +0800

    add solution for leetcode #150 in python

---
+++ leetcode0150/solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        current = 0
+
+        while current < len(tokens):
+            if self.is_a_number(tokens[current]):
+                stack.append(int(tokens[current]))
+            else:
+                v2 = stack.pop()
+                v1 = stack.pop()
+                match tokens[current]:
+                    case "+":
+                        stack.append(v1 + v2)
+                    case "-":
+                        stack.append(v1 - v2)
+                    case "*":
+                        stack.append(v1 * v2)
+                    case "/":
+                        stack.append(int(v1 / v2))
+                    case _:
+                        pass
+            current += 1
+
+
+        return stack[0]
+
+    def is_a_number(self, x):
+        try:
+            int(x)
+            return True
+        except ValueError:
+            return False
