5f59b4c18c1c5739c14d943df804c7b259a7cea5
root <lintimo1999@gmail.com>
2024-01-30 09:02:58 +0800

    update solution for leetcode #150 in python

--- leetcode0150/solution.py
+++ leetcode0150/solution.py
@@ -4,8 +4,10 @@ class Solution:
 
         current = 0
 
+        operators = ["+", "-", "*", "/"]
+
         while current < len(tokens):
-            if self.is_a_number(tokens[current]):
+            if tokens[current] not in operators:
                 stack.append(int(tokens[current]))
             else:
                 v2 = stack.pop()
@@ -26,9 +28,37 @@ class Solution:
 
         return stack[0]
 
-    def is_a_number(self, x):
-        try:
-            int(x)
-            return True
-        except ValueError:
-            return False
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+#         stack = []
+
+#         current = 0
+
+#         while current < len(tokens):
+#             if self.is_a_number(tokens[current]):
+#                 stack.append(int(tokens[current]))
+#             else:
+#                 v2 = stack.pop()
+#                 v1 = stack.pop()
+#                 match tokens[current]:
+#                     case "+":
+#                         stack.append(v1 + v2)
+#                     case "-":
+#                         stack.append(v1 - v2)
+#                     case "*":
+#                         stack.append(v1 * v2)
+#                     case "/":
+#                         stack.append(int(v1 / v2))
+#                     case _:
+#                         pass
+#             current += 1
+
+
+#         return stack[0]
+
+#     def is_a_number(self, x):
+#         try:
+#             int(x)
+#             return True
+#         except ValueError:
+#             return False
