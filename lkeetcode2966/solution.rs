182e6edf89774a49cc850411ebddbb550d901831
root <lintimo1999@gmail.com>
2024-02-01 10:18:21 +0800

    update solution for leetcode #2966 in rust

--- lkeetcode2966/solution.rs
+++ lkeetcode2966/solution.rs
@@ -2,28 +2,40 @@ impl Solution {
     pub fn divide_array(mut nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
         nums.sort_unstable();
 
-        let mut ans: Vec<Vec<i32>> = Vec::new();
-
-        let mut current: usize = 0;
-
-        while current < nums.len() {
-            let mut tmp: Vec<i32> = Vec::new();
-
-            if nums[current + 2] - nums[current] > k {
-                return Vec::new();
-            }
-
-            for i in 0..3 {
-                let i = i as usize;
-
-                tmp.push(nums[current + i]);
-            }
-
-            ans.push(tmp);
-
-            current += 3;
+        if nums.chunks(3).map(|c| c[2] - c[0]).max().unwrap() > k {
+            return Vec::new();
         }
 
-        ans
+        nums.chunks(3).map(|c| c.to_vec()).collect()
     }
 }
+
+// impl Solution {
+//     pub fn divide_array(mut nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+//         nums.sort_unstable();
+
+//         let mut ans: Vec<Vec<i32>> = Vec::new();
+
+//         let mut current: usize = 0;
+
+//         while current < nums.len() {
+//             let mut tmp: Vec<i32> = Vec::new();
+
+//             if nums[current + 2] - nums[current] > k {
+//                 return Vec::new();
+//             }
+
+//             for i in 0..3 {
+//                 let i = i as usize;
+
+//                 tmp.push(nums[current + i]);
+//             }
+
+//             ans.push(tmp);
+
+//             current += 3;
+//         }
+
+//         ans
+//     }
+// }
