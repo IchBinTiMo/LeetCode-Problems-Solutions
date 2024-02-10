c6a69b3d09217ca208d8052db358cc92ad2b4d94
root <lintimo1999@gmail.com>
2024-02-10 09:37:59 +0800

    update solution for leetcode #647 in rust

--- leetcode0647/solution.rs
+++ leetcode0647/solution.rs
@@ -14,9 +14,7 @@ impl Solution {
             let mut left: usize = i;
             let mut right: usize = i;
 
-            ans += 1;
-
-            while left > 0 && right < n - 1 && s[left - 1] == s[right + 1] {
+            while left < n && right < n && s[left] == s[right] {
                 left -= 1;
                 right += 1;
                 ans += 1;
@@ -35,3 +33,41 @@ impl Solution {
         ans
     }
 }
+
+// impl Solution {
+//     pub fn count_substrings(s: String) -> i32 {
+//         if s.len() == 1 {
+//             return 1;
+//         }
+
+//         let mut ans: i32 = 0;
+
+//         let s: Vec<u8> = s.bytes().collect();
+
+//         let n: usize = s.len();
+
+//         for i in 0..n {
+//             let mut left: usize = i;
+//             let mut right: usize = i;
+
+//             ans += 1;
+
+//             while left > 0 && right < n - 1 && s[left - 1] == s[right + 1] {
+//                 left -= 1;
+//                 right += 1;
+//                 ans += 1;
+//             }
+
+//             left = i;
+//             right = i + 1;
+
+//             while left < n && right < n && s[left] == s[right] {
+//                 ans += 1;
+//                 left -= 1;
+//                 right += 1;
+//             }
+//         }
+
+//         ans
+//     }
+// }
