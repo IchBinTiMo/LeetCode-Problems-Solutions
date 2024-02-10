8ee9ab27d0915fc0b91571c2083b326e07bdfef8
root <lintimo1999@gmail.com>
2024-02-10 09:38:17 +0800

    update solution for leetcode #647 in rust

--- leetcode0647/solution.rs
+++ leetcode0647/solution.rs
@@ -1,33 +1,25 @@
 impl Solution {
     pub fn count_substrings(s: String) -> i32 {
-        if s.len() == 1 {
-            return 1;
-        }
-
         let mut ans: i32 = 0;
 
         let s: Vec<u8> = s.bytes().collect();
 
+        for i in 0..s.len() {
+            ans += Solution::expand(&s, i, i) + Solution::expand(&s, i, i + 1);
+        }
+
+        ans
+    }
+
+    fn expand(s: &Vec<u8>, mut left: usize, mut right: usize) -> i32 {
+        let mut ans: i32 = 0;
+
         let n: usize = s.len();
 
-        for i in 0..n {
-            let mut left: usize = i;
-            let mut right: usize = i;
-
-            while left < n && right < n && s[left] == s[right] {
-                left -= 1;
-                right += 1;
-                ans += 1;
-            }
-
-            left = i;
-            right = i + 1;
-
-            while left < n && right < n && s[left] == s[right] {
-                ans += 1;
-                left -= 1;
-                right += 1;
-            }
+        while left < n && right < n && s[left] == s[right] {
+            left -= 1;
+            right += 1;
+            ans += 1;
         }
 
         ans
@@ -50,9 +42,7 @@ impl Solution {
 //             let mut left: usize = i;
 //             let mut right: usize = i;
 
-//             ans += 1;
-
-//             while left > 0 && right < n - 1 && s[left - 1] == s[right + 1] {
+//             while left < n && right < n && s[left] == s[right] {
 //                 left -= 1;
 //                 right += 1;
 //                 ans += 1;
@@ -71,3 +61,41 @@ impl Solution {
 //         ans
 //     }
 // }
+
+// // impl Solution {
+// //     pub fn count_substrings(s: String) -> i32 {
+// //         if s.len() == 1 {
+// //             return 1;
+// //         }
+
+// //         let mut ans: i32 = 0;
+
+// //         let s: Vec<u8> = s.bytes().collect();
+
+// //         let n: usize = s.len();
+
+// //         for i in 0..n {
+// //             let mut left: usize = i;
+// //             let mut right: usize = i;
+
+// //             ans += 1;
+
+// //             while left > 0 && right < n - 1 && s[left - 1] == s[right + 1] {
+// //                 left -= 1;
+// //                 right += 1;
+// //                 ans += 1;
+// //             }
+
+// //             left = i;
+// //             right = i + 1;
+
+// //             while left < n && right < n && s[left] == s[right] {
+// //                 ans += 1;
+// //                 left -= 1;
+// //                 right += 1;
+// //             }
+// //         }
+
+// //         ans
+// //     }
+// // }
