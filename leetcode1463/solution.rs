0ad038637a70f0e170f9ed942bb6e1a13f4e8afd
root <lintimo1999@gmail.com>
2024-02-11 15:40:19 +0800

    update solution for leetcode #1463 in rust

--- leetcode1463/solution.rs
+++ leetcode1463/solution.rs
@@ -3,38 +3,75 @@ impl Solution {
         let m: usize = grid.len();
         let n: usize = grid[0].len();
 
-        let mut prev: Vec<Vec<i32>> = vec![vec![-1; n]; n];
+        let mut dp: Vec<Vec<Vec<i32>>> = vec![vec![vec![-1; n]; n]; m];
 
-        prev[0][n - 1] = grid[0][0] + grid[0][n - 1];
+        dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+
+        let mut ans: i32 = dp[0][0][n - 1];
 
         for i in 1..m {
-            let mut dp: Vec<Vec<i32>> = vec![vec![-1; n]; n];
-
             for j in 0..n {
-                for k in (j + 1)..n {
+                for k in 0..n {
+                    let prev = dp[i - 1][j][k];
 
-                    for dj in 0..3 {
-                        for dk in 0..3 {
-                            let prev_val = *prev.get(j + dj - 1).unwrap_or(&vec![-1_i32; n]).get(k + dk - 1).unwrap_or(&(-1));
-                            if prev_val != -1 {
-                                dp[j][k] = dp[j][k].max(prev_val + grid[i][j] + if j == k {0} else {grid[i][k]});
+                    if prev != -1 {
+                        for dj in 0..3 {
+                            for dk in 0..3 {
+                                let c1 = j + dj - 1;
+                                let c2 = k + dk - 1;
+
+                                if c1 < c2 && c2 < n {
+                                    dp[i][c1][c2] = dp[i][c1][c2].max(prev + grid[i][c1] + if c1 == c2 {0} else {grid[i][c2]});
+                                    ans = ans.max(dp[i][c1][c2]);
+                                }
                             }
                         }
                     }
                 }
-            }
-
-            prev = dp;
-            
-        }
-        let mut ans: i32 = i32::MIN;
-
-        for j in 0..n {
-            for k in 0.. n {
-                ans = ans.max(prev[j][k]);
             }
         }
 
         ans
     }
 }
+
+// impl Solution {
+//     pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
+//         let m: usize = grid.len();
+//         let n: usize = grid[0].len();
+
+//         let mut prev: Vec<Vec<i32>> = vec![vec![-1; n]; n];
+
+//         prev[0][n - 1] = grid[0][0] + grid[0][n - 1];
+
+//         for i in 1..m {
+//             let mut dp: Vec<Vec<i32>> = vec![vec![-1; n]; n];
+
+//             for j in 0..n {
+//                 for k in (j + 1)..n {
+
+//                     for dj in 0..3 {
+//                         for dk in 0..3 {
+//                             let prev_val = *prev.get(j + dj - 1).unwrap_or(&vec![-1_i32; n]).get(k + dk - 1).unwrap_or(&(-1));
+//                             if prev_val != -1 {
+//                                 dp[j][k] = dp[j][k].max(prev_val + grid[i][j] + if j == k {0} else {grid[i][k]});
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+
+//             prev = dp;
+            
+//         }
+//         let mut ans: i32 = i32::MIN;
+
+//         for j in 0..n {
+//             for k in 0.. n {
+//                 ans = ans.max(prev[j][k]);
+//             }
+//         }
+
+//         ans
+//     }
+// }
