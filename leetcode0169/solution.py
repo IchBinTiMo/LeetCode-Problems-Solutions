0a8c1bbd0ee292b0422484550c43bade909f3095
root <lintimo1999@gmail.com>
2024-02-13 08:45:03 +0800

    update solution for leetcode #169 in python

--- leetcode0169/solution.py
+++ leetcode0169/solution.py
@@ -1,16 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0] 
-        nums.sort()
-        count = 1
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                count += 1
-                if count > len(nums) // 2:
-                    return nums[i]
-            else:
-                count = 1
+        c = Counter(nums)
+        l = len(nums) // 2
+
+        for k, v in c.items():
+            if v > l:
+                return k
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         if len(nums) == 1:
+#             return nums[0] 
+#         nums.sort()
+#         count = 1
+#         for i in range(len(nums) - 1):
+#             if nums[i] == nums[i + 1]:
+#                 count += 1
+#                 if count > len(nums) // 2:
+#                     return nums[i]
+#             else:
+#                 count = 1
             
 
         
