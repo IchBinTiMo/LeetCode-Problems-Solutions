c3d4b2576eb549bfb0052a8e7a9adf3d37a7b2f8
root <lintimo1999@gmail.com>
2024-02-16 12:40:40 +0800

    update solution for leetcode #1481 in rust

--- leetcode1481/solution.rs
+++ leetcode1481/solution.rs
@@ -8,20 +8,50 @@ impl Solution {
             ht.entry(num).and_modify(|val| *val += 1).or_insert(1);
         }
 
-        let mut keys: Vec<i32> = ht.keys().map(|k| *k).collect();
+        let mut counts: Vec<i32> = ht.values().map(|v| *v).collect();
 
-        keys.sort_unstable_by_key(|key| -ht.get(key).unwrap());
+        counts.sort_unstable_by_key(|cnt| -cnt);
 
-        while k > 0 {
-            let key = keys.last().unwrap();
-            if let Some(&cnt) = ht.get(key) {
-                if k >= cnt {
-                    keys.pop();
-                }
-                k -= cnt;
+        while let Some(&cnt) = counts.last() {
+            if k >= cnt {
+                counts.pop();
+            }
+
+            k -= cnt;
+
+            if k < 0 {
+                break;
             }
         }
 
-        keys.len() as i32
+        counts.len() as i32
     }
 }
+
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn find_least_num_of_unique_ints(arr: Vec<i32>, mut k: i32) -> i32 {
+//         let mut ht: HashMap<i32, i32> = HashMap::new();
+
+//         for &num in arr.iter() {
+//             ht.entry(num).and_modify(|val| *val += 1).or_insert(1);
+//         }
+
+//         let mut keys: Vec<i32> = ht.keys().map(|k| *k).collect();
+
+//         keys.sort_unstable_by_key(|key| -ht.get(key).unwrap());
+
+//         while k > 0 {
+//             let key = keys.last().unwrap();
+//             if let Some(&cnt) = ht.get(key) {
+//                 if k >= cnt {
+//                     keys.pop();
+//                 }
+//                 k -= cnt;
+//             }
+//         }
+
+//         keys.len() as i32
+//     }
+// }
