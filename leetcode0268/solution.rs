e8740a2c1ed5b51618b7b0abed85e34b5bdce5f1
root <lintimo1999@gmail.com>
2024-02-20 14:23:13 +0800

    update solution for leetcode #268 in rust

--- leetcode0268/solution.rs
+++ leetcode0268/solution.rs
@@ -1,11 +1,21 @@
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let mut sum: i32 = (0..=nums.len()).fold(0, |acc, num| acc + num) as i32;
+        let mut sum: i32 = (nums.len() * (nums.len() + 1) / 2) as i32;
 
-        for num in nums {
-            sum -= num;
-        }
+        sum -= nums.iter().sum::<i32>();
 
         sum
     }
 }
+
+// impl Solution {
+//     pub fn missing_number(nums: Vec<i32>) -> i32 {
+//         let mut sum: i32 = (0..=nums.len()).fold(0, |acc, num| acc + num) as i32;
+
+//         for num in nums {
+//             sum -= num;
+//         }
+
+//         sum
+//     }
+// }
