c636b1782184f38134fa1aa943affd1ace5a9dd4
root <lintimo1999@gmail.com>
2024-03-02 09:38:38 +0800

    update solution for leetcode #977 in rust

--- leetcode0977/solution.rs
+++ leetcode0977/solution.rs
@@ -5,3 +5,11 @@ impl Solution {
         nums.iter().map(|e| e * e).collect::<Vec<i32>>()
     }
 }
+
+// impl Solution {
+//     pub fn sorted_squares(mut nums: Vec<i32>) -> Vec<i32> {
+//         nums.sort_unstable_by_key(|k| k.abs());
+
+//         nums.iter().map(|e| e * e).collect::<Vec<i32>>()
+//     }
+// }
