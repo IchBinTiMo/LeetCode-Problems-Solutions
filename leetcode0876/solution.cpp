b83e5bbc1ec454c530d2b8343ada34ab014f570a
root <lintimo1999@gmail.com>
2024-03-07 11:45:27 +0800

    update solution for leetcode #876 in c++

--- leetcode0876/solution.cpp
+++ leetcode0876/solution.cpp
@@ -11,21 +11,46 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *fast, *slow = head;
+        ListNode *fast = head;
+        ListNode *slow = head;
 
-        while(true) {
-            fast = fast->next;
-            if(fast == NULL) {
-                return slow;
-            }
-
+        while(fast != NULL && fast->next != NULL) {
             slow = slow->next;
-
-            fast = fast->next;
-
-            if(fast == NULL) {
-                return slow;
-            }
+            fast = fast->next->next;
         }
+
+        return slow;
     }
 };
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode *fast, *slow = head;
+
+//         while(true) {
+//             fast = fast->next;
+//             if(fast == NULL) {
+//                 return slow;
+//             }
+
+//             slow = slow->next;
+
+//             fast = fast->next;
+
+//             if(fast == NULL) {
+//                 return slow;
+//             }
+//         }
+//     }
+// };
