865b89022b2e9b8b6a298485c3927c1eb23b807c
root <lintimo1999@gmail.com>
2024-03-08 12:00:37 +0800

    add solution for leetcode #3005 in rust

---
+++ leetcode3005/solution.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn max_frequency_elements(nums: Vec<i32>) -> i32 {
+        let mut freqs: [i32; 100] = [0; 100];
+
+        for num in nums {
+            freqs[(num as usize) - 1] += 1;
+        }
+
+        let mut res: i32 = 0;
+        let mut max_freq: i32 = i32::MIN;
+
+        for freq in freqs {
+            if freq > max_freq {
+                max_freq = freq;
+                res = max_freq;
+            } else if freq == max_freq {
+                res += freq;
+            } else {
+                continue;
+            }
+        }
+
+        res
+    }
+}
