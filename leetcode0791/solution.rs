e9073d2525480a88782953128f4d2a179f33413a
root <lintimo1999@gmail.com>
2024-03-11 12:35:18 +0800

    update solution for leetcode #791 in rust

--- leetcode0791/solution.rs
+++ leetcode0791/solution.rs
@@ -1,17 +1,46 @@
 impl Solution {
     pub fn custom_sort_string(order: String, s: String) -> String {
-        
-        let mut indices: [usize; 26] = [usize::MAX; 26];
+       let mut occurences: [i32; 26] = [0; 26];
 
-        for (idx, byte) in order.bytes().enumerate() {
-            indices[(byte - b'a') as usize] = idx;
-        }
+       for byte in s.bytes() {
+           occurences[(byte - b'a') as usize] += 1;
+       }
 
-        let mut bytes: Vec<u8> = s.bytes().collect();
+       let mut res: Vec<char> = Vec::new();
 
-        
-        bytes.sort_unstable_by_key(|byte| indices[(byte - b'a') as usize]);
+       for byte in order.bytes() {
+           let idx: usize = (byte - b'a') as usize;
+           while occurences[idx] > 0 {
+               res.push(char::from(byte));
+               occurences[idx] -= 1;
+           }
+       }
 
-        bytes.iter().map(|&byte| char::from(byte)).collect()
+       for i in 0..26 {
+           while occurences[i] > 0 {
+               res.push(char::from((i as u8) + b'a'));
+               occurences[i] -= 1;
+           }
+       }
+
+       res.iter().collect()
     }
 }
+
+// impl Solution {
+//     pub fn custom_sort_string(order: String, s: String) -> String {
+        
+//         let mut indices: [usize; 26] = [usize::MAX; 26];
+
+//         for (idx, byte) in order.bytes().enumerate() {
+//             indices[(byte - b'a') as usize] = idx;
+//         }
+
+//         let mut bytes: Vec<u8> = s.bytes().collect();
+
+        
+//         bytes.sort_unstable_by_key(|byte| indices[(byte - b'a') as usize]);
+
+//         bytes.iter().map(|&byte| char::from(byte)).collect()
+//     }
+// }
