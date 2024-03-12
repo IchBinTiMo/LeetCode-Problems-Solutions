7db2719c0336a785897348e39a3bf838f8669da6
root <lintimo1999@gmail.com>
2024-03-12 13:02:31 +0800

    update solution for leetcode #1171 in rust

--- leetcode1171/solution.rs
+++ leetcode1171/solution.rs
@@ -25,13 +25,13 @@ impl Solution {
 
         let mut current_sum: i32 = 0;
 
-        let mut current_node: Option<Box<ListNode>> = head.clone();
+        let mut current_node: &Option<Box<ListNode>> = &head;
 
         sums.push(0);
 
         set.insert(0);
 
-        while let Some(ref node) = current_node {
+        while let Some(node) = current_node {
             current_sum += node.val;
             if let Some(_) = set.get(&current_sum) {
                 while let Some(&sum) = sums.last() {
@@ -46,7 +46,7 @@ impl Solution {
             sums.push(current_sum);
             set.insert(current_sum);
 
-            current_node = node.next.clone();
+            current_node = &node.next;
         }
 
 
@@ -62,3 +62,68 @@ impl Solution {
         res_head
     }
 }
+
+// // Definition for singly-linked list.
+// // #[derive(PartialEq, Eq, Clone, Debug)]
+// // pub struct ListNode {
+// //   pub val: i32,
+// //   pub next: Option<Box<ListNode>>
+// // }
+// // 
+// // impl ListNode {
+// //   #[inline]
+// //   fn new(val: i32) -> Self {
+// //     ListNode {
+// //       next: None,
+// //       val
+// //     }
+// //   }
+// // }
+
+// use std::collections::HashSet;
+
+// impl Solution {
+//     pub fn remove_zero_sum_sublists(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//         let mut sums: Vec<i32> = Vec::new();
+
+//         let mut set: HashSet<i32> = HashSet::new();
+
+//         let mut current_sum: i32 = 0;
+
+//         let mut current_node: Option<Box<ListNode>> = head.clone();
+
+//         sums.push(0);
+
+//         set.insert(0);
+
+//         while let Some(ref node) = current_node {
+//             current_sum += node.val;
+//             if let Some(_) = set.get(&current_sum) {
+//                 while let Some(&sum) = sums.last() {
+//                     sums.pop();
+//                     set.remove(&sum);
+
+//                     if sum == current_sum {
+//                         break;
+//                     }
+//                 }
+//             }
+//             sums.push(current_sum);
+//             set.insert(current_sum);
+
+//             current_node = node.next.clone();
+//         }
+
+
+//         let mut res_head: Option<Box<ListNode>> = None;
+
+//         while let Some(sum) = sums.pop() {
+//             if let Some(&next_sum) = sums.last() {
+//                 let next = res_head.take();
+//                 res_head = Some(Box::new(ListNode{val: sum - next_sum, next}));
+//             }
+//         }
+
+//         res_head
+//     }
+// }
