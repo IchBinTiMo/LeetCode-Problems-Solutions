d8392bd406be148aa0c27cefad1b5ed943f6f1e5
root <lintimo1999@gmail.com>
2024-03-15 10:13:34 +0800

    update solution for leetcode #238 in rust

--- leetcode0238/solution.rs
+++ leetcode0238/solution.rs
@@ -1,24 +1,45 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
         let n: usize = nums.len();
+        
+        let mut forward: Vec<i32> = vec![1; n];
 
-        let mut forward: Vec<i32> = vec![1; n + 2];
-        let mut backward: Vec<i32> = vec![1; n + 2];
+        let mut backward: i32 = 1;
 
-        for i in 1..=n {
-            forward[i] = forward[i - 1] * nums[i - 1];
+        for i in 1..n {
+            forward[i] = nums[i - 1] * forward[i - 1];
         }
 
-        for i in (1..=n).rev() {
-            backward[i] = backward[i + 1] * nums[i - 1];
+        for i in (0..n).rev() {
+            forward[i] *= backward;
+            backward *= nums[i];
         }
 
-        let mut res: Vec<i32> = Vec::new();
-
-        for i in 1..=n {
-            res.push(forward[i - 1] * backward[i + 1]);
-        }
-
-        res
+        forward
     }
 }
+
+// impl Solution {
+//     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+//         let n: usize = nums.len();
+
+//         let mut forward: Vec<i32> = vec![1; n + 2];
+//         let mut backward: Vec<i32> = vec![1; n + 2];
+
+//         for i in 1..=n {
+//             forward[i] = forward[i - 1] * nums[i - 1];
+//         }
+
+//         for i in (1..=n).rev() {
+//             backward[i] = backward[i + 1] * nums[i - 1];
+//         }
+
+//         let mut res: Vec<i32> = Vec::new();
+
+//         for i in 1..=n {
+//             res.push(forward[i - 1] * backward[i + 1]);
+//         }
+
+//         res
+//     }
+// }
