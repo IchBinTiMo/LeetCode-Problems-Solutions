324f5f0311455f287e0477ef5016ea9b5759b5e3
root <lintimo1999@gmail.com>
2024-03-18 10:35:33 +0800

    update solution for leetcode #452 in rust

--- leetcode0452/solution.rs
+++ leetcode0452/solution.rs
@@ -3,31 +3,56 @@ impl Solution {
         // sort by end
         points.sort_unstable_by_key(|p| p[1]);
 
-        let mut res: i32 = 0;
+        let mut res: i32 = 1;
 
-        // [start, end]
-        let mut prev_overlap: Vec<i32> = vec![i32::MIN, i32::MAX];
+        let mut i: usize = 1;
 
-        while !points.is_empty() {
-            while let Some(point) = points.pop() {
-                // calculate the overlap of current overlap and new point
-                let overlap: Vec<i32> = vec![prev_overlap[0].max(point[0]), prev_overlap[1].min(point[1])];
+        let mut end: i32 = points[0][1];
 
-                // if new point doesn't overlap with previous one
-                if overlap[0] > overlap[1] {
-                    // push new point back and reset previous overlap
-                    prev_overlap = vec![i32::MIN, i32::MAX];
-                    points.push(point);
-                    break;
-                }
-
-                // update previous overlap
-                prev_overlap = overlap;
+        while i < points.len() {
+            // if not overlap then increment res and update end
+            if end < points[i][0] {
+                res += 1;
+                end = points[i][1];
             }
 
-            res += 1;
+            i += 1;
         }
 
         res
     }
 }
+
+// impl Solution {
+//     pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
+//         // sort by end
+//         points.sort_unstable_by_key(|p| p[1]);
+
+//         let mut res: i32 = 0;
+
+//         // [start, end]
+//         let mut prev_overlap: Vec<i32> = vec![i32::MIN, i32::MAX];
+
+//         while !points.is_empty() {
+//             while let Some(point) = points.pop() {
+//                 // calculate the overlap of current overlap and new point
+//                 let overlap: Vec<i32> = vec![prev_overlap[0].max(point[0]), prev_overlap[1].min(point[1])];
+
+//                 // if new point doesn't overlap with previous one
+//                 if overlap[0] > overlap[1] {
+//                     // push new point back and reset previous overlap
+//                     prev_overlap = vec![i32::MIN, i32::MAX];
+//                     points.push(point);
+//                     break;
+//                 }
+
+//                 // update previous overlap
+//                 prev_overlap = overlap;
+//             }
+
+//             res += 1;
+//         }
+
+//         res
+//     }
+// }
