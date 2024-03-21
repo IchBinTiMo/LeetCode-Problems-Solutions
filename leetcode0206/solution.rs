edad6b21381005d4f8613b86f4d4c9ef4abce229
root <lintimo1999@gmail.com>
2024-03-21 10:28:03 +0800

    update solution for leetcode #206 in rust

--- leetcode0206/solution.rs
+++ leetcode0206/solution.rs
@@ -22,6 +22,38 @@ impl Solution {
         while current.is_some() {
             // swap
             let mut tmp: Option<Box<ListNode>> = std::mem::replace(&mut current.as_mut().unwrap().next, prev);
+            prev = (*current).take();
+            *current = tmp;
+        }
+
+        prev
+    }
+}
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut current: &mut Option<Box<ListNode>> = &mut head;
+        let mut prev: Option<Box<ListNode>> = None;
+
+        while current.is_some() {
+            // swap
+            let mut tmp: Option<Box<ListNode>> = std::mem::replace(&mut current.as_mut().unwrap().next, prev);
             prev = (*current).clone();
             *current = tmp;
         }
