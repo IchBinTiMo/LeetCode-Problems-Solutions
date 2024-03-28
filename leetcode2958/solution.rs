8f40faa66d1dedf0b649973b2087386efac1a141
root <lintimo1999@gmail.com>
2024-03-28 11:29:01 +0800

    update solution for leetcode #2958 in rust

--- leetcode2958/solution.rs
+++ leetcode2958/solution.rs
@@ -17,34 +17,77 @@ impl Solution {
                 .or_insert(1);
 
             if let Some(&freq) = ht.get(&nums[right]) {
-                // if 'right' is at the end of the array and the frequencies of all the elements are not greater than k
-                if freq <= k && right == n - 1 {
-                    res = res.max(right - left + 1);
-                }
-                
-                // if 'freq' is greater than 'k' then shrink the window
-                while freq > k {
-                    res = res.max(right - left);
-                    ht.entry(nums[left])
-                        .and_modify(|e| *e -= 1);
-                    
-                    // if the frequency of 'nums[left]' is 0 then remove it from the hashmap
-                    if let Some(&out) = ht.get(&nums[left]) {
-                        if out == 0 {
-                            ht.remove(&nums[left]);
+                // if the frequency of 'nums[right]' is greater than 'k' then shrink the window 
+                // until the frequency of 'nums[right]' is less than or equal to 'k'
+                if freq > k {
+                    loop {
+                        ht.entry(nums[left])
+                            .and_modify(|e| *e -= 1);
+                        
+                        left += 1;
+
+                        if nums[left - 1] == nums[right] {
+                            break;
                         }
                     }
-                    left += 1;
 
-                    // if the element just popped is equal to the element at 'right' then break
-                    if nums[left - 1] == nums[right] {
-                        break;
-                    }
                 }
             }
             right += 1;
+            res = res.max(right - left);
         }
 
         res as i32
     }
 }
+
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn max_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
+//         let n: usize = nums.len();
+
+//         let mut res: usize = 0;
+
+//         let mut left: usize = 0;
+//         let mut right: usize = 0;
+
+//         let mut ht: HashMap<i32, i32> = HashMap::new();
+
+//         while right < n {
+//             ht.entry(nums[right])
+//                 .and_modify(|e| *e += 1)
+//                 .or_insert(1);
+
+//             if let Some(&freq) = ht.get(&nums[right]) {
+//                 // if 'right' is at the end of the array and the frequencies of all the elements are not greater than k
+//                 if freq <= k && right == n - 1 {
+//                     res = res.max(right - left + 1);
+//                 }
+                
+//                 // if 'freq' is greater than 'k' then shrink the window
+//                 while freq > k {
+//                     res = res.max(right - left);
+//                     ht.entry(nums[left])
+//                         .and_modify(|e| *e -= 1);
+                    
+//                     // if the frequency of 'nums[left]' is 0 then remove it from the hashmap
+//                     if let Some(&out) = ht.get(&nums[left]) {
+//                         if out == 0 {
+//                             ht.remove(&nums[left]);
+//                         }
+//                     }
+//                     left += 1;
+
+//                     // if the element just popped is equal to the element at 'right' then break
+//                     if nums[left - 1] == nums[right] {
+//                         break;
+//                     }
+//                 }
+//             }
+//             right += 1;
+//         }
+
+//         res as i32
+//     }
+// }
