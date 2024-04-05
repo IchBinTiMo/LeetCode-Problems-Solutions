a4d528d7900ae1eb830700e82cad77cd99e792ed
root <lintimo1999@gmail.com>
2024-04-05 09:59:14 +0800

    add solution for leetcode #1544 in rust

---
+++ leetcode1544/solution.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn make_good(mut s: String) -> String {
+        if s.len() <= 1 {
+            return s;
+        }
+
+        let mut stack: Vec<char> = Vec::new();
+
+        while let Some(c) = s.pop() {
+            if let Some(last) = stack.pop() {
+                if c != last && c.to_lowercase().eq(last.to_lowercase()) {
+                    continue;
+                } else {
+                    stack.push(last);
+                    stack.push(c);
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        stack.iter().rev().map(|&e| e as char).collect::<String>()
+    }
+}
