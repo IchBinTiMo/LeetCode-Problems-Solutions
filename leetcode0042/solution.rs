885e6d1574a7e93cb18f4f03743d8834ac6c9eb7
root <lintimo1999@gmail.com>
2024-04-12 14:02:00 +0800

    update solution for leetcode #42 in rust

--- leetcode0042/solution.rs
+++ leetcode0042/solution.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-
+        /// Time O(N) | Space O(1)
         let n: usize = height.len();
 
         if n <= 2 {
@@ -9,14 +9,16 @@ impl Solution {
 
         let mut ans: i32 = 0;
 
-        let mut maxL: i32 = height[0];
-        let mut maxR: i32 = height[n - 1];
+        let mut maxL: i32 = height[0]; // max height of left wall
+        let mut maxR: i32 = height[n - 1]; // max height of right wall
 
         let mut left: usize = 1;
         let mut right: usize = n - 2;
 
         while left <= right {
+            // compare max height of left wall and right wall
             if maxL < maxR {
+                // if current height of left wall is higher then update maxL, otherwise increase ans by maxL - height[left]
                 if height[left] > maxL {
                     maxL = height[left];
                 } else {
@@ -24,7 +26,8 @@ impl Solution {
                 }
 
                 left += 1;
-            } else {
+            } else { 
+                // if current height of right wall is higher then update maxL, otherwise increase ans by maxR - height[right]
                 if height[right] > maxR {
                     maxR = height[right];
                 } else {
