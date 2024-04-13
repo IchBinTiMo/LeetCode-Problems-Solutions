4484fe0fa7dabdaca87a63f61f799b84012f921b
root <lintimo1999@gmail.com>
2024-04-13 13:47:37 +0800

    update solution for leetcode #84 in rust

--- leetcode0084/solution.rs
+++ leetcode0084/solution.rs
@@ -1,14 +1,14 @@
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        let mut stack = Vec::new();
-        let mut max_area = 0;
+        let mut stack: Vec<usize> = Vec::new();
+        let mut max_area: i32 = 0;
         for i in 0..=heights.len() {
             while !stack.is_empty() && (i == heights.len() || heights[i] < heights[*stack.last().unwrap()]) {
                 let height = heights[stack.pop().unwrap()];
                 let width = if stack.is_empty() {
                     i as i32
                 } else {
-                    i as i32 - *stack.last().unwrap() as i32 - 1
+                    (i - *stack.last().unwrap() - 1) as i32
                 };
                 max_area = max_area.max(height * width);
             }
