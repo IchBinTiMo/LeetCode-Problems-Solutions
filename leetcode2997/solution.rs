85e41a2242fdc8257d2afc2cfc3472db206830f3
root <lintimo1999@gmail.com>
2024-04-29 11:55:01 +0800

    update solution for leetcode #2997 in rust

--- leetcode2997/solution.rs
+++ leetcode2997/solution.rs
@@ -1,7 +1,13 @@
 impl Solution {
     pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-        let mut diff: i32 = (1..nums.len()).fold(nums[0], |acc, i| acc ^ nums[i]) ^ k;
-
-        diff.count_ones() as i32
+        nums.iter().fold(k, |acc, &num| acc ^ num).count_ones() as i32
     }
 }
+
+// impl Solution {
+//     pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+//         let mut diff: i32 = (1..nums.len()).fold(nums[0], |acc, i| acc ^ nums[i]) ^ k;
+
+//         diff.count_ones() as i32
+//     }
+// }
