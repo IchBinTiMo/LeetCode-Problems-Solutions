b65084006fe18b557e13358f72a7f4d6c5612566
root <lintimo1999@gmail.com>
2024-04-30 11:25:53 +0800

    update solution for leetcode #1915 in rust

--- leetcode1915/solution.rs
+++ leetcode1915/solution.rs
@@ -1,34 +1,30 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn wonderful_substrings(word: String) -> i64 {
+        /// brute force but replace hashmap with vector
         let mut res: i64 = 0;
 
-        let mut masks: HashMap<i32, i64> = HashMap::new();
+        let mut masks: Vec<i64> = vec![0; 1024];
 
-        let mut current_mask: i32 = 0;
+        let mut current_mask: usize = 0;
 
-        masks.insert(0, 1);
+        masks[0] = 1;
 
         for c in word.bytes() {
             current_mask ^= 1 << ((c - b'a') as i32);
 
+            res += masks[current_mask];
+
             // find all masks that is differs from current mask by exactly 1 bit
             // which means current mask ^ these prefix mask == 1
             for i in 0..10 {
-                if let Some(&count) = masks.get(&(current_mask ^ (1 << i))) {
-                    res += count;
-                }
+                res += masks[(current_mask ^ (1 << i))];
             }
 
-            masks.entry(current_mask).and_modify(|mask| *mask += 1).or_insert(1);
-
             // current mask ^ this prefix mask == 0
-            res += masks[&current_mask] - 1;
+            masks[current_mask] += 1;
         }
 
         res
-
     }
 }
 
@@ -45,21 +41,56 @@ impl Solution {
 
 //         masks.insert(0, 1);
 
-//         for c in word.chars() {
-//             let c: i32 = (c as i32) - ('a' as i32);
+//         for c in word.bytes() {
+//             current_mask ^= 1 << ((c - b'a') as i32);
 
-//             current_mask ^= 1 << c;
-
-//             for key in masks.keys() {
-//                 if (key ^ current_mask).count_ones() < 2 {
-//                     res += masks[key];
+//             // find all masks that is differs from current mask by exactly 1 bit
+//             // which means current mask ^ these prefix mask == 1
+//             for i in 0..10 {
+//                 if let Some(&count) = masks.get(&(current_mask ^ (1 << i))) {
+//                     res += count;
 //                 }
 //             }
 
 //             masks.entry(current_mask).and_modify(|mask| *mask += 1).or_insert(1);
+
+//             // current mask ^ this prefix mask == 0
+//             res += masks[&current_mask] - 1;
 //         }
 
 //         res
 
 //     }
 // }
+
+// // use std::collections::HashMap;
+
+// // impl Solution {
+// //     pub fn wonderful_substrings(word: String) -> i64 {
+// //         /// brute force
+// //         let mut res: i64 = 0;
+
+// //         let mut masks: HashMap<i32, i64> = HashMap::new();
+
+// //         let mut current_mask: i32 = 0;
+
+// //         masks.insert(0, 1);
+
+// //         for c in word.chars() {
+// //             let c: i32 = (c as i32) - ('a' as i32);
+
+// //             current_mask ^= 1 << c;
+
+// //             for key in masks.keys() {
+// //                 if (key ^ current_mask).count_ones() < 2 {
+// //                     res += masks[key];
+// //                 }
+// //             }
+
+// //             masks.entry(current_mask).and_modify(|mask| *mask += 1).or_insert(1);
+// //         }
+
+// //         res
+
+// //     }
+// // }
