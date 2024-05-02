822147c529381377b7d3f02ad3c87e98fef4ef42
root <lintimo1999@gmail.com>
2024-05-02 12:52:18 +0800

    update solution for leetcode #2441 in rust

--- leetcode2441/solution.rs
+++ leetcode2441/solution.rs
@@ -1,23 +1,48 @@
 impl Solution {
     pub fn find_max_k(mut nums: Vec<i32>) -> i32 {
-        /// Two Pointers
+        /// Hash
         /// 
-        /// Time: O(n log n) | Space: O(1)
-        nums.sort_unstable();
+        /// Time: O(n) | Space: O(n)
+        let mut visited: Vec<bool> = vec![false; 2001];
 
-        let mut left: usize = 0;
-        let mut right: usize = nums.len() - 1;
+        let mut res: i32 = -1;
 
-        while left < right {
-            if nums[left] == -nums[right] {
-                return nums[right];
-            } else if nums[left] < -nums[right] {
-                left += 1;
-            } else {
-                right -= 1;
+        for num in nums {
+            visited[(1000 + num) as usize] = true;
+
+            if visited[(1000 - num) as usize] {
+                if num > 0 {
+                    res = res.max(num);
+                } else {
+                    res = res.max(-num);
+                }
             }
         }
 
-        -1
+        res
     }
 }
+
+// impl Solution {
+//     pub fn find_max_k(mut nums: Vec<i32>) -> i32 {
+//         /// Two Pointers
+//         /// 
+//         /// Time: O(n log n) | Space: O(1)
+//         nums.sort_unstable();
+
+//         let mut left: usize = 0;
+//         let mut right: usize = nums.len() - 1;
+
+//         while left < right {
+//             if nums[left] == -nums[right] {
+//                 return nums[right];
+//             } else if nums[left] < -nums[right] {
+//                 left += 1;
+//             } else {
+//                 right -= 1;
+//             }
+//         }
+
+//         -1
+//     }
+// }
