b2d6ca7d7035e6fb6f5af5af44f71afdae87f5d0
root <lintimo1999@gmail.com>
2024-05-04 12:45:39 +0800

    update solution for leetcode #881 in rust

--- leetcode0881/solution.rs
+++ leetcode0881/solution.rs
@@ -13,20 +13,10 @@ impl Solution {
         let mut res: i32 = 0;
 
         while right < n && left <= right {
-            let mut sum: i32 = 0;
-            let mut count: i32 = 0;
-
-            while right < n && limit - sum >= people[right] && count < 2 {
-                sum += people[right];
-                right -= 1;
-                count += 1;
-            }
-
-            while left < n && limit - sum >= people[left] && count < 2 {
-                sum += people[left];
+            if people[left] + people[right] <= limit {
                 left += 1;
-                count += 1;
             }
+            right -= 1;
 
             res += 1;
         }
@@ -34,3 +24,40 @@ impl Solution {
         res
     }
 }
+
+// impl Solution {
+//     pub fn num_rescue_boats(mut people: Vec<i32>, limit: i32) -> i32 {
+//         /// Two Pointers
+//         /// 
+//         /// Time: O(nlogn) | Space: O(1)
+//         people.sort_unstable();
+
+//         let n: usize = people.len();
+
+//         let mut left: usize = 0;
+//         let mut right: usize = n - 1;
+
+//         let mut res: i32 = 0;
+
+//         while right < n && left <= right {
+//             let mut sum: i32 = 0;
+//             let mut count: i32 = 0;
+
+//             while right < n && limit - sum >= people[right] && count < 2 {
+//                 sum += people[right];
+//                 right -= 1;
+//                 count += 1;
+//             }
+
+//             while left < n && limit - sum >= people[left] && count < 2 {
+//                 sum += people[left];
+//                 left += 1;
+//                 count += 1;
+//             }
+
+//             res += 1;
+//         }
+
+//         res
+//     }
+// }
