31408e3a9c92e85559985aec3807d2de5d4419f7
root <lintimo1999@gmail.com>
2024-05-16 11:06:34 +0800

    add solution for leetcode #2331 in rust

---
+++ leetcode2331/solution.rs
@@ -0,0 +1,50 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn evaluate_tree(mut root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        /// DFS
+        /// 
+        /// Time O(N) | Space O(H)
+        let mut ops: Vec<i32> = Vec::new();
+
+        Self::traversal(root, &mut ops);
+        
+        ops.pop().unwrap() != 0
+    }
+
+    fn traversal(root: Option<Rc<RefCell<TreeNode>>>, ops: &mut Vec<i32>) {
+        if let Some(node) = root {
+            let mut node = node.borrow_mut();
+
+            Self::traversal(node.left.take(), ops);
+            Self::traversal(node.right.take(), ops);
+            
+            if node.val == 2 || node.val == 3 {
+                let right: i32 = ops.pop().unwrap();
+                let left: i32 = ops.pop().unwrap();
+
+                ops.push(if node.val == 2 {left | right} else {left & right});
+            } else {
+                ops.push(node.val);
+            }
+        }
+    }
+}
