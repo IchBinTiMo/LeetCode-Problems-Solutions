171a7efadf8663b00cac7a0bd96d65b9afd5914b
root <lintimo1999@gmail.com>
2024-05-18 15:52:18 +0800

    update solution for leetcode #834 in rust

--- leetcode0834/solution.rs
+++ leetcode0834/solution.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn sum_of_distances_in_tree(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+        /// DFS
+        /// 
+        /// Time: O(n) | Space: O(n)
         let n: usize = n as usize;
 
         let mut res: Vec<i32> = vec![0; n];
@@ -9,6 +12,7 @@ impl Solution {
 
         let mut graph: HashMap<usize, Vec<usize>> = HashMap::new();
 
+        // build graph
         for edge in edges.iter() {
             let u: usize = edge[0] as usize;
             let v: usize = edge[1] as usize;
@@ -27,6 +31,7 @@ impl Solution {
 
         prev.push(0);
 
+        // bfs to find the sum of distances starting from node 0
         loop {
             let mut current: Vec<usize> = Vec::new();
 
@@ -56,6 +61,7 @@ impl Solution {
 
         prev.push(0);
 
+        // bfs to find the sum of distances starting from each node
         loop {
             let mut current: Vec<usize> = Vec::new();
 
@@ -65,6 +71,9 @@ impl Solution {
                 if let Some(nexts) = graph.get(&node) {
                     for &next in nexts.iter() {
                         if !visited[next] {
+                            // sum[j] = sum[i] - count[j, i] + count[i, j]
+                            //where count[i, j] is the number of nodes in subtree with node i if edge[i, j] was removed
+                            //count[j, i] = number of nodes in the whole tree - count[i, j]
                             res[next] = res[node] + (n as i32) - count[next] * 2;
                             current.push(next);
                         }
@@ -82,6 +91,7 @@ impl Solution {
         res
     }
 
+    // dfs to count the number of nodes below the current node (including the current node)
     fn count_nodes(current: usize, prev: usize, graph: &HashMap<usize, Vec<usize>>, count: &mut Vec<i32>) {
         count[current] = 1;
         if let Some(nexts) = graph.get(&current) {
