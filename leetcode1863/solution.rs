9d20c94a0077174be63bd27865d444ad86aa312a
root <lintimo1999@gmail.com>
2024-05-20 09:14:37 +0800

    add solution for leetcode #1863 in rust

---
+++ leetcode1863/solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        let mut res: i32 = 0;
+
+        Self::backtracking(&mut res, 0, 0, &nums);
+
+        res
+    }
+
+    fn backtracking(res: &mut i32, path: i32, current: usize, nums: &Vec<i32>) {
+        *res += path;
+
+        for i in current..nums.len() {
+            Self::backtracking(res, path ^ nums[i], i + 1, nums);
+        }
+    }
+}
