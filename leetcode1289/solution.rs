b2b57416854add851f7be18f9c897642d0fec91a
root <lintimo1999@gmail.com>
2024-05-28 21:01:41 +0800

    update solution to leetcode #1289 in rust

--- leetcode1289/solution.rs
+++ leetcode1289/solution.rs
@@ -1,8 +1,8 @@
 impl Solution {
     pub fn min_falling_path_sum(grid: Vec<Vec<i32>>) -> i32 {
-        /// DP without optimization
+        /// DP with optimization
         /// 
-        /// Time O(n^3) | Space O(n)
+        /// Time O(n^2) | Space O(n)
         /// where n is the length of grid
         let n: usize = grid.len();
 
@@ -11,11 +11,14 @@ impl Solution {
         for i in 1..n {
             let mut current: Vec<i32> = vec![i32::MAX; n];
 
+            let (min, min2) = Self::get_min(&prev);
+
+
             for j in 0..n {
-                for k in 0..n {
-                    if j != k {
-                        current[j] = current[j].min(prev[k] + grid[i][j]);
-                    }
+                if prev[j] == min {
+                    current[j] = grid[i][j] + min2;
+                } else {
+                    current[j] = grid[i][j] + min;
                 }
             }
 
@@ -24,4 +27,47 @@ impl Solution {
 
         *prev.iter().min().unwrap()
     }
+
+    fn get_min(row: &[i32]) -> (i32, i32) {
+        let (mut min, mut min2): (i32, i32) = (i32::MAX, i32::MAX);
+
+        for i in 0..row.len() {
+            if row[i] < min {
+                min2 = min;
+                min = row[i];
+            } else if row[i] < min2 {
+                    min2 = row[i];
+            }
+        }
+
+        (min, min2)
+    }
 }
+
+// impl Solution {
+//     pub fn min_falling_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+//         /// DP without optimization
+//         /// 
+//         /// Time O(n^3) | Space O(n)
+//         /// where n is the length of grid
+//         let n: usize = grid.len();
+
+//         let mut prev: Vec<i32> = grid[0].clone();
+
+//         for i in 1..n {
+//             let mut current: Vec<i32> = vec![i32::MAX; n];
+
+//             for j in 0..n {
+//                 for k in 0..n {
+//                     if j != k {
+//                         current[j] = current[j].min(prev[k] + grid[i][j]);
+//                     }
+//                 }
+//             }
+
+//             prev = current;
+//         }
+
+//         *prev.iter().min().unwrap()
+//     }
+// }
