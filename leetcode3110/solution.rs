a9b3ebfcf60f8fc3a84bdbb9ef86c77ce09e7699
root <lintimo1999@gmail.com>
2024-06-01 13:19:34 +0800

    update solution to leetcode #3110 in rust

--- leetcode3110/solution.rs
+++ leetcode3110/solution.rs
@@ -1,8 +1,17 @@
 impl Solution {
     pub fn score_of_string(s: String) -> i32 {
         /// Time: O(N) | Space: O(N)
-        let s: Vec<char> = s.chars().collect::<Vec<char>>();
+        let s: Vec<i32> = s.chars().map(|c| c as i32).collect::<Vec<i32>>();
 
-        (1..s.len()).fold(0, |acc, idx| acc + ((s[idx] as i32) - (s[idx - 1] as i32)).abs())
+        (1..s.len()).fold(0, |acc, idx| acc + (s[idx] - s[idx - 1]).abs())
     }
 }
+
+// impl Solution {
+//     pub fn score_of_string(s: String) -> i32 {
+//         /// Time: O(N) | Space: O(N)
+//         let s: Vec<char> = s.chars().collect::<Vec<char>>();
+
+//         (1..s.len()).fold(0, |acc, idx| acc + ((s[idx] as i32) - (s[idx - 1] as i32)).abs())
+//     }
+// }
