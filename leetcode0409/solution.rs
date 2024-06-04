c7841ea89212d275a3ceb6aafae61a3a4af6207d
root <lintimo1999@gmail.com>
2024-06-04 08:23:03 +0800

    add solution to leetcode #409 in rust

---
+++ leetcode0409/solution.rs
@@ -0,0 +1,35 @@
+impl Solution {
+    pub fn longest_palindrome(s: String) -> i32 {
+        /// Time O(n) | Space O(1)
+        /// where n is the length of the string
+        let mut freqs: [i32; 58] = [0; 58];
+
+        for c in s.chars() {
+            freqs[(c as usize) - 65] += 1;
+        }
+
+        // there is at most one odd frequency in a palindrome
+        // false means the frequencies of all characters in the palindrome are even
+        let mut one: bool = false;
+
+        let mut res: i32 = 0;
+
+        for freq in freqs {
+            if freq == 0 {
+                continue;
+            }
+
+            if one {
+                // there is one character with odd frequency in the palindrome
+                res += freq / 2 * 2;
+            } else {
+                res += freq;
+                if freq & 1 == 1 {
+                    one = true;
+                }
+            }
+        }
+
+        res
+    }
+}
