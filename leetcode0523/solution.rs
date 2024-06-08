4ea79fdcad7ec3bf438b18cb81a3942cf08efb73
root <lintimo1999@gmail.com>
2024-06-08 14:13:43 +0800

    add solution to leetcode #523 in rust

---
+++ leetcode0523/solution.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+        /// Prefix sum
+        /// 
+        /// Time: O(n) | Space: O(n)
+        /// where n is the length of nums
+        let n: usize = nums.len();
+
+        if n < 2 {
+            return false;
+        }
+
+        let mut prefix: Vec<i32> = vec![nums[0] % k];
+
+        // calculate the remainder of the prefix sum divided by k
+        for i in 1..n {
+            prefix.push((prefix[i - 1] + nums[i]) % k);
+        }
+
+        let mut visited: HashMap<i32, i32> = HashMap::new();
+
+        visited.insert(0, -1);
+
+        // if the remainde occurs more than once
+        //, which means the sum of the subarray from `idx` to `i` is a multiple of k,
+        // and the distance between them is greater than 1
+        // then the subarray exists
+        for i in 0..n {
+            if let Some(idx) = visited.get(&prefix[i]) {
+                if (i as i32) - idx > 1 {
+                    return true;
+                }
+            } else {
+                visited.insert(prefix[i], i as i32);
+            }
+        }
+
+        false
+
+    }
+}
