f0d3906bafaf0575c81d556dafc15bad0f9b3905
root <lintimo1999@gmail.com>
2024-06-14 11:16:58 +0800

    add solution to leetcode #945 in rust

---
+++ leetcode0945/solution.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn min_increment_for_unique(nums: Vec<i32>) -> i32 {
+        /// Time O(n + 200001) | Space O(200001)
+        /// where n is the length of nums
+        
+        let mut freqs: [i32; 200001] = [0; 200001];
+        let mut min: i32 = i32::MAX; // the index we should start from
+
+        for &num in nums.iter() {
+            freqs[num as usize] += 1;
+            min = min.min(num);
+        }
+
+        let mut res: i32 = 0;
+
+        let mut i: usize = min as usize;
+        let mut cnt: usize = 0; // to count how many numbers we have seen, in order to know when to stop
+
+        while cnt < nums.len() {
+            let mut freq: i32 = freqs[i];
+
+            cnt += if freq == 0 {0} else {1};
+
+            if freq > 1 {
+                freqs[i + 1] += freq - 1;
+                res += freq - 1;
+            }
+
+            i += 1;
+        }
+
+        res
+    }
+}
