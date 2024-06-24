9c88f52d20decd53f4329601ad63de1525f21a61
root <lintimo1999@gmail.com>
2024-06-24 19:17:41 +0800

    update solution to leetcode #995 in rust

--- leetcode0995/solution.rs
+++ leetcode0995/solution.rs
@@ -1,10 +1,9 @@
-use std::collections::VecDeque;
 
 impl Solution {
     pub fn min_k_bit_flips(mut nums: Vec<i32>, k: i32) -> i32 {
         /// Slide Window
         /// 
-        /// Time: O(n) | Space: O(n)
+        /// Time: O(n) | Space: O(1)
         /// where n is the length of nums
         let n: usize = nums.len();
 
@@ -12,15 +11,13 @@ impl Solution {
 
         let mut res: i32 = 0;
 
-        let mut left: VecDeque<usize> = VecDeque::new(); // the leftmost index of each window
         let mut is_flipped: i32 = 0; // whether the current window is flipped
 
         for right in 0..n {
-            while left.front().is_some_and(|l| right - l >= k) {
+            if right >= k && nums[right - k] == 2 {
                 // leave the leftmost window
                 // and subtract flipping times by 1
                 // which is the same as toggle 'is_flipped'
-                left.pop_front();
                 is_flipped ^= 1;
             }
 
@@ -31,7 +28,7 @@ impl Solution {
                     // so return -1
                     return -1;
                 }
-                left.push_back(right);
+                nums[right] = 2;
                 // increase flipping times by 1
                 // which is the same as toggle 'is_flipped'
                 is_flipped ^= 1;
