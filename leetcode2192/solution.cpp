061a9e90e72ee604e2e51c6fc9f4712ed5832d3f
root <lintimo1999@gmail.com>
2024-06-29 14:28:18 +0800

    update solution to leetcode #2285 in c++

--- leetcode2192/solution.cpp
+++ leetcode2192/solution.cpp
@@ -1,62 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        /* BFS
-        *
-        * Time: O(E + V) | Space: O(E + V)
-        * E: Number of edges
-        * V: Number of vertices */
+        /*DFS
+        
+        Time: O(E + V) | Space: O(E + V)
+        E: Number of edges
+        V: Number of vertices
+        */
         vector<vector<int>> adj(n);
 
         for (auto &edge: edges) {
             int from = edge[0];
             int to = edge[1];
 
-            adj[to].push_back(from);
+            adj[from].push_back(to);
         }
 
         vector<vector<int>> res(n);
 
-        // BFS
         for (int i = 0; i < n; ++i) {
-            deque<int> dq;
-            vector<bool> visited(n, false);
-
-            dq.push_back(i);
-
-            while (!dq.empty()) {
-                int current = dq.front();
-
-                if (!visited[current]) {
-                    visited[current] = true;
-                    if (current != i) {
-                        res[i].push_back(current);
-                    }
-
-                    for (auto &node: adj[current]) {
-                        dq.push_back(node);
-                    }
-
-                }
-
-                dq.pop_front();
-            }
-        }
-
-        // Sort
-        for (int i = 0; i < n; ++i) {
-            sort(res[i].begin(), res[i].end());
+            DFS(adj, res, i, i);
         }
 
         return res;
+    }
 
-
+    void DFS(vector<vector<int>>& adj, vector<vector<int>>& res, int& parent, int& child) {
+        for (auto &c: adj[child]) {
+            if (res[c].size() == 0 || res[c].back() != parent) {
+                res[c].push_back(parent);
+                DFS(adj, res, parent, c);
+            }
+        }
     }
 };
 
 // class Solution {
 // public:
 //     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+//         /* BFS
+//         *
+//         * Time: O(E + V) | Space: O(E + V)
+//         * E: Number of edges
+//         * V: Number of vertices */
 //         vector<vector<int>> adj(n);
 
 //         for (auto &edge: edges) {
@@ -66,8 +52,9 @@ public:
 //             adj[to].push_back(from);
 //         }
 
-//         vector<set<int>> ancs(n);
+//         vector<vector<int>> res(n);
 
+//         // BFS
 //         for (int i = 0; i < n; ++i) {
 //             deque<int> dq;
 //             vector<bool> visited(n, false);
@@ -76,24 +63,26 @@ public:
 
 //             while (!dq.empty()) {
 //                 int current = dq.front();
-//                 visited[current] = true;
 
-//                 for (auto &node: adj[current]) {
-//                     if (!visited[node]) {
-//                         dq.push_back(node);
-//                         ancs[i].insert(node);
+//                 if (!visited[current]) {
+//                     visited[current] = true;
+//                     if (current != i) {
+//                         res[i].push_back(current);
 //                     }
+
+//                     for (auto &node: adj[current]) {
+//                         dq.push_back(node);
+//                     }
+
 //                 }
 
 //                 dq.pop_front();
 //             }
 //         }
 
-//         vector<vector<int>> res(n);
-
+//         // Sort
 //         for (int i = 0; i < n; ++i) {
-//             vector<int> vec(ancs[i].begin(), ancs[i].end());
-//             res[i] = vec;
+//             sort(res[i].begin(), res[i].end());
 //         }
 
 //         return res;
@@ -101,3 +90,51 @@ public:
 
 //     }
 // };
+
+// // class Solution {
+// // public:
+// //     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+// //         vector<vector<int>> adj(n);
+
+// //         for (auto &edge: edges) {
+// //             int from = edge[0];
+// //             int to = edge[1];
+
+// //             adj[to].push_back(from);
+// //         }
+
+// //         vector<set<int>> ancs(n);
+
+// //         for (int i = 0; i < n; ++i) {
+// //             deque<int> dq;
+// //             vector<bool> visited(n, false);
+
+// //             dq.push_back(i);
+
+// //             while (!dq.empty()) {
+// //                 int current = dq.front();
+// //                 visited[current] = true;
+
+// //                 for (auto &node: adj[current]) {
+// //                     if (!visited[node]) {
+// //                         dq.push_back(node);
+// //                         ancs[i].insert(node);
+// //                     }
+// //                 }
+
+// //                 dq.pop_front();
+// //             }
+// //         }
+
+// //         vector<vector<int>> res(n);
+
+// //         for (int i = 0; i < n; ++i) {
+// //             vector<int> vec(ancs[i].begin(), ancs[i].end());
+// //             res[i] = vec;
+// //         }
+
+// //         return res;
+
+
+// //     }
+// // };
