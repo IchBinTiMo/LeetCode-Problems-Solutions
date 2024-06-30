6fe82a67886844cbb89ac1ae4043e6e0af6d8dad
root <lintimo1999@gmail.com>
2024-06-30 16:34:57 +0800

    update solution to leetcode #1579 in c++

--- leetcode1579/solution.cpp
+++ leetcode1579/solution.cpp
@@ -13,43 +13,52 @@ public:
 
         set<tuple<int, int, int>> to_remove;
 
-        priority_queue<tuple<int, int, int>> pq_a, pq_b; // this is why there is a V log V in Time Complexity
+        priority_queue<tuple<int, int, int>> pq; // this is why there is a V log V in Time Complexity
 
         for (auto& edge: edges) {
             int type = edge[0];
             int from = edge[1];
             int to = edge[2];
 
-            if (type == 1) {
-                pq_a.push({type, from - 1, to - 1});
-            } else if (type == 2) {
-                pq_b.push({type, from - 1, to - 1});
-            } else {
-                pq_a.push({type, from - 1, to - 1});
-                pq_b.push({type, from - 1, to - 1});
-            }
+            pq.push({type, from - 1, to - 1});
         }
 
-        int remain_a = 0, remain_b = 0;
+        int to_remove = 0;
 
-        while (!pq_a.empty()) {
-            auto [type, from, to] = pq_a.top();
+        while (!pq.empty()) {
+            auto [type, x, y] = pq.top();
 
-            int pf = find(parent_a, from); 
-            int pt = find(parent_a, to);
+            int px_a = find(parent_a, x); // parent of x in Alice's tree
+            int py_a = find(parent_a, y); // parent of y in Alice's tree
+            int px_b = find(parent_b, x); // parent of x in Bob's tree
+            int py_b = find(parent_b, y); // parent of y in Bob's tree
 
-            if (pf == pt) {
-                to_remove.insert({type, from, to});
-            } else {
-                if (size_a[pf] < size_a[pt]) {
-                    parent_a[pf] = pt;
-                    size_a[pt] += size_a[pf];
+            if (type == 3) {
+                if (px_a == py_a && px_b == py_b) {
+                    to_remove += 1;
                 } else {
-                    parent_a[pt] = pf;
-                    size_a[pf] += size_a[pt];
+                    if (px_a != py_a) {
+                        unionn(parent_a, size_a, px_a, py_a);
+                    }
+                    if (px_b != py_b) {
+                        unionn(parent_b, size_b, px_b, py_b);
+                    }
+                }
+            } else if (type == 2) {
+                if (px_b == py_b) {
+                    to_remove += 1;
+                } else {
+                    unionn(parent_b, size_b, px_b, py_b);
+                }
+            } else {
+                if (px_a == py_a) {
+                    to_remove += 1;
+                } else {
+                    unionn(parent_a, size_a, px_a, py_a);
                 }
             }
-            pq_a.pop();
+
+            pq.pop();
         }
 
         while (!pq_b.empty()) {
@@ -100,6 +109,16 @@ public:
             return current;
         } else {
             return find(parent, parent[current]);
+        }
+    }
+
+    void unionn(vector<int>& parent, vector<int>& size, int x, int y) {
+        if (size[x] < size[y]) {
+            parent[x] = y;
+            size[y] += size[x];
+        } else {
+            parent[y] = x;
+            size[x] += size[y];
         }
     }
 };
