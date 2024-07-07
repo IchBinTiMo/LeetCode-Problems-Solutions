6323d32685e185a508d93ec92dc117c09f623fb7
root <lintimo1999@gmail.com>
2024-07-07 13:40:38 +0800

    update solution to leetcode #1518 in c++

--- leetcode1518/solution.cpp
+++ leetcode1518/solution.cpp
@@ -6,18 +6,37 @@ public:
         
         n: numBottles
         */
-        int res = 0;
-        int rem = numBottles % numExchange;
+        int res = numBottles;
 
-        while (numBottles / numExchange > 0) {
-            res += numBottles - rem;
-            numBottles /= numExchange;
-            numBottles += rem;
-            rem = numBottles % numExchange;
+        while (numBottles >= numExchange) {
+            res += numBottles / numExchange;
+            numBottles = numBottles / numExchange + numBottles % numExchange;
         }
-
-        res += rem;
 
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int numWaterBottles(int numBottles, int numExchange) {
+//         /*
+//         Time: O( log n ) | Space: O( 1 )
+        
+//         n: numBottles
+//         */
+//         int res = 0;
+//         int rem = numBottles % numExchange;
+
+//         while (numBottles / numExchange > 0) {
+//             res += numBottles - rem;
+//             numBottles /= numExchange;
+//             numBottles += rem;
+//             rem = numBottles % numExchange;
+//         }
+
+//         res += rem;
+
+//         return res;
+//     }
+// };
