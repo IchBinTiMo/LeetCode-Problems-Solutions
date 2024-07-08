ca3da313e2d1c8b7df380faaaefb2bd981dd1699
root <lintimo1999@gmail.com>
2024-07-08 15:04:21 +0800

    update solution to leetcode #1823 in c++

--- leetcode1823/solution.cpp
+++ leetcode1823/solution.cpp
@@ -3,36 +3,60 @@ public:
     int findTheWinner(int n, int k) {
         /*
         Brute force solution
-        Time O(n * k) | Space O(n)
+        Time O(n * n) | Space O(n)
         */
-        vector<bool> visited(n, false);
-
+        vector<int> vec;
         int current = 0;
-        int lose = 0;
-
-        while (n - lose > 1) {
-            int step = 0;
-
-            while (step < k) {
-                if (!visited[(current + step) % n]) {
-                    step += 1;
-                } else {
-                    current += 1;
-                }
-            }
-
-            visited[(current + step - 1) % n] = true;
-            current = (current + step) % n;
-            lose += 1;
-            
-        }
 
         for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                return i + 1;
-            }
+            vec.push_back(i + 1);
         }
 
-        return 1;
+        while (vec.size() > 1) {
+            current = (current + k - 1) % n;
+            vec.erase(vec.begin() + current); // this is why the time complexity is O(n * n)
+            n -= 1;
+        }
+
+        return vec[0];
     }
 };
+
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) {
+//         /*
+//         Brute force solution
+//         Time O(n * k) | Space O(n)
+//         */
+//         vector<bool> visited(n, false);
+
+//         int current = 0;
+//         int lose = 0;
+
+//         while (n - lose > 1) {
+//             int step = 0;
+
+//             while (step < k) {
+//                 if (!visited[(current + step) % n]) {
+//                     step += 1;
+//                 } else {
+//                     current += 1;
+//                 }
+//             }
+
+//             visited[(current + step - 1) % n] = true;
+//             current = (current + step) % n;
+//             lose += 1;
+            
+//         }
+
+//         for (int i = 0; i < n; ++i) {
+//             if (!visited[i]) {
+//                 return i + 1;
+//             }
+//         }
+
+//         return 1;
+//     }
+// };
