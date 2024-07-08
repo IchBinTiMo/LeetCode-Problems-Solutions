d318594451b65fbd780b32c1ca5d419723ce4e87
root <lintimo1999@gmail.com>
2024-07-08 15:07:10 +0800

    update solution to leetcode #1823 in c++

--- leetcode1823/solution.cpp
+++ leetcode1823/solution.cpp
@@ -1,43 +1,36 @@
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        /* Recursion % DP
-        Time O(n) | Space O(n)
+        /* Optimized DP
+        Time O(n) | Space O(1)
         */
-        return recursion(n, k) + 1;
-    }
+        int res = 0;
 
-    int recursion(int n, int k) {
-        /* Space Complexity = O(n) is due to the call stack */
-        if (n == 1) {
-            return 0;
-        } else {
-            return (recursion(n - 1, k) + k) % n;
+        for (int i = 2; i <= n; ++i) {
+            res = (res + k) % i;
         }
+
+        return res + 1;
     }
 };
 
 // class Solution {
 // public:
 //     int findTheWinner(int n, int k) {
-//         /*
-//         Brute force solution
-//         Time O(n * n) | Space O(n)
+//         /* Recursion % DP
+//         Time O(n) | Space O(n)
 //         */
-//         vector<int> vec;
-//         int current = 0;
+//         return recursion(n, k) + 1;
+//     }
 
-//         for (int i = 0; i < n; ++i) {
-//             vec.push_back(i + 1);
+//     int recursion(int n, int k) {
+//         /* Space Complexity = O(n) is due to the call stack */
+//         if (n == 1) {
+//             return 0;
+//         } else {
+//             return (recursion(n - 1, k) + k) % n;
 //         }
-
-//         while (vec.size() > 1) {
-//             current = (current + k - 1) % n;
-//             vec.erase(vec.begin() + current); // this is why the time complexity is O(n * n)
-//             n -= 1;
-//         }
-
-//         return vec[0];
 //     }
 // };
 
@@ -46,36 +39,60 @@ public:
 // //     int findTheWinner(int n, int k) {
 // //         /*
 // //         Brute force solution
-// //         Time O(n * k) | Space O(n)
+// //         Time O(n * n) | Space O(n)
 // //         */
-// //         vector<bool> visited(n, false);
-
+// //         vector<int> vec;
 // //         int current = 0;
-// //         int lose = 0;
-
-// //         while (n - lose > 1) {
-// //             int step = 0;
-
-// //             while (step < k) {
-// //                 if (!visited[(current + step) % n]) {
-// //                     step += 1;
-// //                 } else {
-// //                     current += 1;
-// //                 }
-// //             }
-
-// //             visited[(current + step - 1) % n] = true;
-// //             current = (current + step) % n;
-// //             lose += 1;
-            
-// //         }
 
 // //         for (int i = 0; i < n; ++i) {
-// //             if (!visited[i]) {
-// //                 return i + 1;
-// //             }
+// //             vec.push_back(i + 1);
 // //         }
 
-// //         return 1;
+// //         while (vec.size() > 1) {
+// //             current = (current + k - 1) % n;
+// //             vec.erase(vec.begin() + current); // this is why the time complexity is O(n * n)
+// //             n -= 1;
+// //         }
+
+// //         return vec[0];
 // //     }
 // // };
+
+// // // class Solution {
+// // // public:
+// // //     int findTheWinner(int n, int k) {
+// // //         /*
+// // //         Brute force solution
+// // //         Time O(n * k) | Space O(n)
+// // //         */
+// // //         vector<bool> visited(n, false);
+
+// // //         int current = 0;
+// // //         int lose = 0;
+
+// // //         while (n - lose > 1) {
+// // //             int step = 0;
+
+// // //             while (step < k) {
+// // //                 if (!visited[(current + step) % n]) {
+// // //                     step += 1;
+// // //                 } else {
+// // //                     current += 1;
+// // //                 }
+// // //             }
+
+// // //             visited[(current + step - 1) % n] = true;
+// // //             current = (current + step) % n;
+// // //             lose += 1;
+            
+// // //         }
+
+// // //         for (int i = 0; i < n; ++i) {
+// // //             if (!visited[i]) {
+// // //                 return i + 1;
+// // //             }
+// // //         }
+
+// // //         return 1;
+// // //     }
+// // // };
