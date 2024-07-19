1a76dfa453171cfa04d8b6980d64131160943991
root <lintimo1999@gmail.com>
2024-07-19 10:33:53 +0800

    add solution to leetcode #104 in c++

---
+++ leetcode0104/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        /*
+        DFS
+
+        Time: O(n) | Space: O(n)
+
+        where n is the number of nodes in the tree
+        */
+        if (root == NULL) {
+            return 0;
+        }
+
+        int l = maxDepth(root->left);
+        int r = maxDepth(root->right);
+
+        return 1 + max(l, r);
+    }
+};
