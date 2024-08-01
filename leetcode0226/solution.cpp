4785bb89946b68d472dc80cc3ac658e23bb1b987
root <lintimo1999@gmail.com>
2024-08-02 01:21:52 +0800

    add solution to leetcode #226 in c++

---
+++ leetcode0226/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+Solution 1:
+
+DFS
+
+Time: O(n) | Space: O(n)
+
+- n: the number of nodes in the tree
+*/
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root) {
+            swap(root->left, root->right);
+
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
+};
