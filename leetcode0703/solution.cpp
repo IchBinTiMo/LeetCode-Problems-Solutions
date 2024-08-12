e6261f98ca26058e645f45ba6847bdf4fdce17fd
root <lintimo1999@gmail.com>
2024-08-12 08:49:45 +0800

    update solution to leetcode #703 in c++

--- leetcode0703/solution.cpp
+++ leetcode0703/solution.cpp
@@ -1,5 +1,45 @@
 /*
-Solution 1: Brute Force
+Solution 1: Priority Queue
+
+Time: O(m * log(n)) | Space: O(n)
+
+- m: # of calls
+- n: length of nums
+*/
+
+class KthLargest {
+public:
+    int count;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    KthLargest(int k, vector<int>& nums) {
+        count = k;
+        for (auto &num: nums) {
+            pq.push(num);
+            if (pq.size() > count) {
+                pq.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > count) {
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+/*
+Solution 2: Brute Force
 
 Time: O(m * n) | Space: O(n)
 
