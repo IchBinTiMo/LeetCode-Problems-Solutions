4c941e4776ad037a26044e84fa101e26b04120eb
root <lintimo1999@gmail.com>
2024-08-18 14:27:50 +0800

    update solution to leetcode #264 in c++

--- leetcode0264/solution.cpp
+++ leetcode0264/solution.cpp
@@ -1,10 +1,62 @@
 /*
-Solution 1: brute force
+Solution 1: DP
+
+Time: O(n) | Space: O(n)
+
+Runtime: 0344 ms, faster than 100.00%
+Memory Usage: 9.69 MB, less than 72.38%
+
+- n: length of 'n'
+*/
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n + 1, 0);
+
+        dp[1] = 1;
+
+        int two = 1;
+        int three = 1;
+        int five = 1;
+
+        int index = 2;
+
+        while (index <= n) {
+            int two_v = dp[two] * 2;
+            int three_v = dp[three] * 3;
+            int five_v = dp[five] * 5;
+
+            int mini = min({two_v, three_v, five_v});
+
+            dp[index] = mini;
+
+            if (mini == two_v) {
+                two += 1;
+            }
+            if (mini == three_v) {
+                three += 1;
+            }
+            if (mini == five_v) {
+                five += 1;
+            }
+
+            index += 1;
+        }
+
+        return dp[n];
+    }
+};
+
+/*
+Solution 2: brute force
 
 Time: O(n) | Space: O(n)
 
 Runtime: 344 ms, faster than 5.05%
 Memory Usage: 93.01 MB, less than 5.10%
+
+- n: length of 'n'
 */
 
 class Solution {
