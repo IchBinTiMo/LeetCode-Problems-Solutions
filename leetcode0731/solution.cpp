eebc56304559652724193ca147a5a00c5c25335b
root <lintimo1999@gmail.com>
2024-09-27 10:09:51 +0800

    update solution to leetcode #731 in c++

--- leetcode0731/solution.cpp
+++ leetcode0731/solution.cpp
@@ -1,10 +1,45 @@
 /*
-Solution: Sweep Line
+Solution 1: Brute Force
 
 Time: O(n ^ 2) | Space: O(n)
 
-Runtime: 0 ms | 100.00%
-Memory: 6.9 MB | 100.00%
+Runtime: 57 ms | 98.85%
+Memory: 37.86 MB | 96.56%
+
+- n: # of operations called on 'MyCalendarTwo'
+*/
+
+class MyCalendarTwo {
+    vector<pair<int,int>> b;
+    vector<pair<int,int>>db;
+public:
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+        for(pair<int,int> x: db){
+            if(start<x.second && end>x.first) return false;
+        }
+        for(pair<int,int> x : b){
+            if(start<x.second && end>x.first){
+                db.push_back({max(start,x.first),min(end,x.second)});
+            }
+        }
+        b.push_back({start,end});
+        return true;
+        
+    }
+};
+
+/*
+Solution 2: Sweep Line
+
+Time: O(n ^ 2) | Space: O(n)
+
+Runtime: 151 ms | 71.11%
+Memory: 42.66 MB | 46.51%
 
 - n: # of operations called on 'MyCalendarTwo'
 */
