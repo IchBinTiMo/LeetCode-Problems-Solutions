9f75cc924d857f361acf9ce90d1a60d55e499819
root <lintimo1999@gmail.com>
2024-10-02 08:49:01 +0800

    add solution to leetcode #1331 in rust

---
+++ leetcode1331/solution.rs
@@ -0,0 +1,44 @@
+/*
+Solution:
+
+Time: O(n log n) | Space: O(n)
+
+Runtime: 18 ms | 74.36%
+Memory: 4.64 MB | 35.90%
+
+- n: length of arr
+*/
+
+impl Solution {
+    pub fn array_rank_transform(arr: Vec<i32>) -> Vec<i32> {
+        if arr.is_empty() {
+            return Vec::new()
+        }
+        
+        let n: usize = arr.len();
+        let mut indices: Vec<(usize, i32)> = Vec::new();
+
+        let mut current: i32 = 1;
+
+        for i in 0..n {
+            indices.push((i, 0));
+        }
+
+        indices.sort_unstable_by_key(|idx| arr[idx.0]);
+
+        indices[0].1 = 1;
+
+        for i in 1..n {
+            indices[i].1 = if arr[indices[i].0] == arr[indices[i - 1].0] {
+                current
+            } else {
+                current += 1;
+                current
+            };
+        }
+
+        indices.sort_unstable_by_key(|idx| idx.0);
+
+        indices.into_iter().map(|(old, new)| new as i32).collect::<Vec<i32>>()
+    }
+}
