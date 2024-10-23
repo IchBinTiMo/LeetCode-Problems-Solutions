afdde2610963f06d9ce525ddf1a0d78ee85a980b
root <lintimo1999@gmail.com>
2024-10-23 12:02:32 +0800

    update solution to leetcode #2641 in c++

--- leetcode2641/solution.cpp
+++ leetcode2641/solution.cpp
@@ -17,6 +17,8 @@ Time: O(n) | Space: O(n)
 
 Runtime: 10 ms | 98.66%
 Memory: 314.59 MB | 28.95%
+
+- n: the number of nodes in the tree
 */
 class Solution {
 public:
@@ -61,5 +63,64 @@ public:
         replace(current + 1, root->left);
         replace(current + 1, root->right);
         
+    }
+};
+
+/*
+Solution 2: BFS
+
+Time: O(n) | Space: O(n)
+
+Runtime: 47 ms | 95.32%
+Memory: 328.92 MB | 23.16%
+
+- n: the number of nodes in the tree
+*/
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        bfs(root);
+
+        root->val = 0;
+
+        return root;
+    }
+
+    void bfs(TreeNode* root) {
+        vector<TreeNode*> prev;
+
+        prev.push_back(root);
+
+        while (!prev.empty()) {
+            vector<TreeNode*> next;
+
+            int sum = 0;
+
+            for (auto &node: prev) {
+                sum += node->left == NULL ? 0 : node->left->val;
+                sum += node->right == NULL ? 0 : node->right->val;
+            }
+
+            while (!prev.empty()) {
+                TreeNode* node = prev.back();
+
+                prev.pop_back();
+
+                int left = node->left == NULL ? 0 : node->left->val;
+                int right = node->right == NULL ? 0 : node->right->val;
+
+                if (node->left) {
+                    node->left->val = sum - left - right;
+                    next.push_back(node->left);
+                }
+
+                if (node->right) {
+                    node->right->val = sum - left - right;
+                    next.push_back(node->right);
+                }
+
+            }
+            prev = next;
+        }
     }
 };
