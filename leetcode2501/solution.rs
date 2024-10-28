af17d99c21e0d1894ea15567db1430146f059938
root <lintimo1999@gmail.com>
2024-10-28 08:53:45 +0800

    update solution to leetcode #2501 in rust

--- leetcode2501/solution.rs
+++ leetcode2501/solution.rs
@@ -1,5 +1,51 @@
 /*
-Solution 1: HashMap
+Solution 1: DP
+
+Time: O(n log n) | Space: O(n)
+
+Runtime: 10 ms | 100.00%
+Memory: 3.7 MB | 100.00%
+
+- n: length of 'nums'
+*/
+
+impl Solution {
+    pub fn longest_square_streak(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable();
+
+        let mut res: i32 = -1;
+
+        let mut dp: Vec<i32> = vec![-1; 100001];
+        let mut prev: i32 = -1;
+
+        for &num in nums.iter() {
+            if prev == num {
+                continue;
+            } else {
+                prev = num;
+            }
+
+            let num: usize = num as usize;
+
+            if num * num <= 100001 {
+                dp[num * num] = num as i32;
+            }
+
+            if dp[num] > 1 {
+                dp[num] = dp[dp[num] as usize] + 1;
+
+                res = res.max(dp[num]);
+            } else {
+                dp[num] = 1;
+            }
+        }
+
+        res
+    }
+}
+
+/*
+Solution 2: HashMap
 
 Time: O(n log n) | Space: O(n)
 
