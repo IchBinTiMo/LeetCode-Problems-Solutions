b6a32cf874aa63c646d354e6a97fab9564188247
root <lintimo1999@gmail.com>
2025-02-10 18:16:33 -0700

    update solution to leetcode #1910 in golang

--- leetcode1910/solution.go
+++ leetcode1910/solution.go
@@ -3,6 +3,27 @@ Solution: Stack
 
 Time: O(n) | Space: O(n)
 
+Runtime: 0 ms | 100.00%
+Memory: 4.29 MB | 100.00%
+
+- n: length of 's'
+*/
+
+func removeOccurrences(s string, part string) string {
+
+	index := strings.Index(s, part)
+	for index != -1 {
+		s = s[:index] + s[index+len(part):]
+		index = strings.Index(s, part)
+	}
+	return s
+}
+
+/*
+Solution: Stack
+
+Time: O(n) | Space: O(n)
+
 Runtime: 3 ms | 43.24%
 Memory: 8.44 MB | 29.73%
 
