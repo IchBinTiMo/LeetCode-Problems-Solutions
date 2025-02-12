d3deb889b078bd10ea0f7917ea82bc1e49876d46
root <lintimo1999@gmail.com>
2025-02-11 18:52:20 -0700

    update solution to leetcode #2342 in rust

--- leetcode2342/solution.rs
+++ leetcode2342/solution.rs
@@ -3,6 +3,45 @@ Solution:
 
 Time: O(n) | Space: O(1)
 
+Runtime: 0 ms | 100.00%
+Memory: 4.08 MB | 73.53%
+
+- n: length of 'nums'
+*/
+
+impl Solution {
+    pub fn maximum_sum(nums: Vec<i32>) -> i32 {
+        let mut map: [i32; 100] = [0; 100];
+
+        let mut res: i32 = -1;
+
+        for &num in nums.iter() {
+            let mut n: i32 = num;
+            let mut sum: i32 = 0;
+
+            while n > 0 {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            let sum: usize = sum as usize;
+
+            if map[sum] > 0 {
+                res = res.max(num + map[sum]);
+            }
+
+            map[sum] = map[sum].max(num);
+        }
+
+        res
+    }
+}
+
+/*
+Solution:
+
+Time: O(n) | Space: O(n)
+
 Runtime: 15 ms | 17.65%
 Memory: 4.14 MB | 20.59%
 
