911d75e1b44b129bdd30acbd9096db28e2ff839a
root <lintimo1999@gmail.com>
2025-03-07 19:24:10 -0700

    update solution to leetcode #1780 in rust

---
+++ leetcode1780/solution.rs
@@ -0,0 +1,35 @@
+/*
+Solution: 
+
+Time: O(n) | Space: O(1)
+
+Runtime: 0 ms | 100.00%
+Memory: 2.22 MB | 51.11%
+*/
+
+impl Solution {
+    pub fn check_powers_of_three(mut n: i32) -> bool {
+        let mut visited: i32 = 0;
+
+        let mut current: i32 = 1;
+        let mut power: i32 = 0;
+
+        while current * 3 <= n {
+            current *= 3;
+            power += 1;
+        }
+
+        while current > 0 {
+            if visited & (1 << power) == 0 && n >= current {
+                n -= current;
+                visited &= 1 << power;
+            }
+
+            current /= 3;
+            power -= 1;
+
+        }
+
+        n == 0
+    }
+}
