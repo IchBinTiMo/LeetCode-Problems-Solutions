53454005cdc49b8c90465452a2ec3c10ae61c320
root <lintimo1999@gmail.com>
2025-03-07 19:22:24 -0700

    update solution to leetcode #2579 in rust

--- leetcode2579/solution.rs
+++ leetcode2579/solution.rs
@@ -1,4 +1,21 @@
 /*
+Solution:
+
+Time: O(1) | Space: O(1)
+
+Runtime: 0 ms | 100.00%
+Memory: 2.26 MB | 48.05%
+*/
+
+impl Solution {
+    pub fn colored_cells(n: i32) -> i64 {
+        let n: i64 = n as i64;
+
+        n * n + (n - 1) * (n - 1)
+    }
+}
+
+/*
 Solution: 
 
 Time: O(n) | Space: O(1)
